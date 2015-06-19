254b625d81d87045e087749ff06cb62ff2853090
Josep M. Perez <josep.m.perez@bsc.es>
2015-06-19 19:26:12 +0200

    Added test with one task.
    It verifies that:
    	1. the main task keeps running after adding the task
    	2. the runtime shuts down only after the non-main task has finished

---
+++ src/system/ompss/AddTask.hpp
@@ -0,0 +1,13 @@
+#ifndef ADD_TASK_HPP
+#define ADD_TASK_HPP
+
+
+#include "tasks/Task.hpp"
+
+
+namespace ompss {
+	void addTask(Task *task);
+} // namespace ompss
+
+
+#endif // ADD_TASK_HPP
---
+++ tests/apiless/single-nest/single-nest.cc
@@ -0,0 +1,77 @@
+#include <atomic>
+#include <cassert>
+#include <cstdio>
+#include <sstream>
+
+#include <unistd.h>
+
+#include "system/ompss/AddTask.hpp"
+#include "tests/infrastructure/TestAnyProtocolProducer.hpp"
+
+
+extern TestAnyProtocolProducer tap;
+std::atomic<bool> theOuterTaskHasFinished;
+
+
+void shutdownTests()
+{
+	/* Test2 */ tap.evaluate(theOuterTaskHasFinished, "Evaluating whether the runtime exits after the regular task");
+	
+	assert(theOuterTaskHasFinished);
+	tap.end();
+}
+
+
+class OuterExplicitTask: public Task {
+public:
+	std::atomic<bool> _mainHasFinished;
+	std::atomic<bool> _theTaskHasFinished;
+	
+	OuterExplicitTask():
+		Task(nullptr)
+	{
+	}
+	
+	virtual void body()
+	{
+		long waitIncrement = 1;
+		for (long i=0; i < 8192; i+= waitIncrement) {
+			if (!_mainHasFinished) {
+				std::ostringstream oss;
+				oss << "Still waiting for main to finish after " << i << " ms";
+				/* Test1 */ tap.emitDiagnostic(oss.str());
+			} else {
+				break;
+			}
+			struct timespec ts = { 0, waitIncrement*1000 };
+			int rc = nanosleep(&ts, nullptr);
+			
+			if (rc != 0) {
+				/* Test1 */ tap.failure(std::string(sys_errlist[errno]));
+				/* Test1 */ tap.bailOut();
+				return;
+			}
+		}
+		
+		/* Test1 */ tap.evaluate(_mainHasFinished, "Evaluating within a regular task whether the main task has finished in a reasonable amount of time");
+		
+		theOuterTaskHasFinished = true;
+	}
+};
+
+
+int main(int argc, char **argv)
+{
+	tap.registerNewTests(2);
+	tap.begin();
+	
+	theOuterTaskHasFinished = false;
+	
+	OuterExplicitTask *theOuterTask = new OuterExplicitTask();
+	ompss::addTask(theOuterTask);
+	
+	theOuterTask->_mainHasFinished = true; // Well, almost. It has continued after adding the task
+	
+	return 0;
+}
+
