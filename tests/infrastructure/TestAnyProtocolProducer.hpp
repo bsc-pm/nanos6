589a68b6cc0befa42954f9bab8dcdc469b87852a
Josep M. Perez <josep.m.perez@bsc.es>
2015-07-02 15:17:41 +0200

    Added methods to emit more comprehensive TAP diagnostics.

--- tests/infrastructure/TestAnyProtocolProducer.hpp
+++ tests/infrastructure/TestAnyProtocolProducer.hpp
@@ -162,9 +162,28 @@ public:
 	}
 	
 	//! \brief emit an additional comment in the output
-	void emitDiagnostic(std::string const &diagnostic)
+	template <typename T1>
+	void emitDiagnostic(T1 const &diagnostic)
 	{
 		std::cout << "# " << diagnostic << std::endl;
+	}
+	
+	template <typename T1, typename T2>
+	void emitDiagnostic(T1 const &diagnostic1, T2 const &diagnostic2)
+	{
+		std::cout << "# " << diagnostic1 << diagnostic2 << std::endl;
+	}
+	
+	template <typename T1, typename T2, typename T3>
+	void emitDiagnostic(T1 const &diagnostic1, T2 const &diagnostic2, T3 const &diagnostic3)
+	{
+		std::cout << "# " << diagnostic1 << diagnostic2 << diagnostic3 << std::endl;
+	}
+	
+	template <typename T1, typename T2, typename T3, typename T4>
+	void emitDiagnostic(T1 const &diagnostic1, T2 const &diagnostic2, T3 const &diagnostic3, T4 const &diagnostic4)
+	{
+		std::cout << "# " << diagnostic1 << diagnostic2 << diagnostic3 << diagnostic4 << std::endl;
 	}
 	
 };
