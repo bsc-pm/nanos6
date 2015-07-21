53e28651adb9406c7eeb70873e8885fe5e253820
Josep M. Perez <josep.m.perez@bsc.es>
2015-07-21 17:43:39 +0200

    Added interface for tests to query data structures within the runtime.

---
+++ src/executors/threads/ThreadManagerDebuggingInterface.hpp
@@ -0,0 +1,25 @@
+#ifndef THREAD_MANAGER_DEBUGGING_INTERFACE_HPP
+#define THREAD_MANAGER_DEBUGGING_INTERFACE_HPP
+
+
+#include "ThreadManager.hpp"
+
+
+class ThreadManagerDebuggingInterface {
+public:
+	typedef std::vector<std::atomic<CPU *>> cpu_list_t;
+	
+	static cpu_list_t const &getCPUListConstRef()
+	{
+		return ThreadManager::_cpus;
+	}
+	
+	static cpu_list_t &getCPUListRef()
+	{
+		return ThreadManager::_cpus;
+	}
+	
+};
+
+
+#endif // THREAD_MANAGER_DEBUGGING_INTERFACE_HPP
