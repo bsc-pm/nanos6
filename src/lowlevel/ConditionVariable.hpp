31b57a53cf2223f129d5418e1784027286e8c191
Josep M. Perez <josep.m.perez@bsc.es>
2015-08-24 12:51:26 +0200

    Fixed important bug in the condition variable code.

--- src/lowlevel/ConditionVariable.hpp
+++ src/lowlevel/ConditionVariable.hpp
@@ -51,12 +51,16 @@ public:
 	//! \brief Signal the condition variable to wake up a thread that is waiting or will wait on it
 	void signal()
 	{
-		{
-			std::unique_lock<std::mutex> lock(_mutex);
-			assert(_signaled == false);
-			_signaled = true;
-		}
+		std::unique_lock<std::mutex> lock(_mutex);
+		assert(_signaled == false);
+		_signaled = true;
 		
+		// The lock needs to cover the notification for the following scenario:
+		// 1. Signaler enters the mutual exclusion, sets up the flag and exits the mutual exclusion
+		// 2. Waiter enters the mutual exclusion, checks the flag, resets it, exits the mutual exclusion, and returns without blocking
+		// 3. Waiter calls again the wait method: enters the mutual exclusion, checks the flag, and blocks on the condition variable
+		// 4. The signaler now calls the condition variable notification (that was supposed to be sent togheter with setting the flag)
+		// 5. The waiter gets (wrongly) woken up
 		_condVar.notify_one();
 	}
 	
