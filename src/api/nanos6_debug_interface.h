eaa471e6ec76f37f570832757c5eab064f224b6e
Josep M. Perez <josep.m.perez@bsc.es>
2015-10-06 13:26:06 +0200

    Made the debugging API use standard C symbols instead of C++ ones.

--- src/api/nanos6_debug_interface.h
+++ src/api/nanos6_debug_interface.h
@@ -2,10 +2,17 @@
 #define NANOS6_DEBUG_INTERFACE_H
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 char const *nanos_get_runtime_version();
 char const *nanos_get_runtime_branch();
 char const *nanos_get_runtime_compiler_version();
 char const *nanos_get_runtime_compiler_flags();
 
+#ifdef __cplusplus
+}
+#endif
 
 #endif // NANOS6_DEBUG_INTERFACE_H
