33d304ed837f722d01e4731032f2e9e0effa1323
Josep M. Perez <josep.m.perez@bsc.es>
2015-10-30 17:55:42 +0100

    Added helper class to propagate constness from one type to another.

---
+++ src/support/ConstPropagator.hpp
@@ -0,0 +1,30 @@
+#ifndef CONST_PROPAGATOR_HPP
+#define CONST_PROPAGATOR_HPP
+
+
+#include <type_traits>
+
+
+namespace const_propagator_internals {
+	template<typename T, bool MAKE_CONST = false>
+	struct ConstPropagatorHelper {
+		typedef T type;
+	};
+	
+	template<typename T>
+	struct ConstPropagatorHelper<T, true> {
+		typedef typename std::add_const<T>::type type;
+	};
+}
+
+
+template<typename SOURCE_T, typename TARGET_T>
+struct ConstPropagator {
+	typedef typename const_propagator_internals::ConstPropagatorHelper<
+		TARGET_T,
+		std::is_const<SOURCE_T>::value
+	>::type type;
+};
+
+
+#endif // CONST_PROPAGATOR_HPP
