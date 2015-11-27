5e6dcfbe8e30e2139cc92440d94e18dc6cb594dc
Josep M. Perez <josep.m.perez@bsc.es>
2015-11-27 16:18:15 +0100

    Minor restructuring of task data access unregistration code.

--- src/dependencies/DataAccessRegistration.hpp
+++ src/dependencies/DataAccessRegistration.hpp
@@ -14,9 +14,70 @@
 #include "tasks/Task.hpp"
 
 #include <InstrumentDependenciesByAccessSequences.hpp>
+#include <InstrumentTaskId.hpp>
 
 
 class DataAccessRegistration {
+private:
+	typedef std::deque<Task *> satisfied_originator_list_t;
+	
+	static inline void unregisterDataAccess(Instrument::task_id_t instrumentationTaskId, DataAccess *dataAccess, satisfied_originator_list_t /* OUT */ &satisfiedOriginators)
+	{
+		DataAccessSequence *dataAccessSequence = dataAccess->_dataAccessSequence;
+		DataAccessSequence::access_sequence_t::iterator dataAccessPosition = dataAccessSequence->_accessSequence.iterator_to(*dataAccess);
+		
+		// Erase the DataAccess and reevaluate if the following ones in the sequence become satisfied
+		// NOTE: This is done with lock held
+		{
+			std::lock_guard<SpinLock> guard(dataAccessSequence->_lock);
+			
+			Instrument::removedDataAccessFromSequence(
+				dataAccessSequence->_instrumentationId,
+				dataAccess->_instrumentationId,
+				instrumentationTaskId
+			);
+			auto nextPosition = dataAccessSequence->_accessSequence.erase(dataAccessPosition);
+			
+			while (nextPosition != dataAccessSequence->_accessSequence.end()) {
+				bool becomesSatisfied = dataAccessSequence->reevaluateSatisfactibility(nextPosition);
+				if (becomesSatisfied) {
+					Instrument::dataAccessBecomesSatisfied(
+						dataAccessSequence->_instrumentationId,
+						nextPosition->_instrumentationId,
+						instrumentationTaskId,
+						nextPosition->_originator->getInstrumentationTaskId()
+					);
+					satisfiedOriginators.push_back(nextPosition->_originator);
+					
+					nextPosition++;
+				} else {
+					// Either it was already satisfied or it cannot become satisfied
+					break;
+				}
+			}
+		}
+	}
+	
+	
+	//! Process all the originators for whose a DataAccess has become satisfied
+	static inline void processSatisfiedOriginators(satisfied_originator_list_t &satisfiedOriginators, HardwarePlace *hardwarePlace)
+	{
+		// NOTE: This is done without the lock held and may be slow since it can enter the scheduler
+		for (Task *satisfiedOriginator : satisfiedOriginators) {
+			assert(satisfiedOriginator != 0);
+			
+			bool becomesReady = satisfiedOriginator->decreasePredecessors();
+			if (becomesReady) {
+				HardwarePlace *idleHardwarePlace = Scheduler::addReadyTask(satisfiedOriginator, hardwarePlace);
+				
+				if (idleHardwarePlace != nullptr) {
+					ThreadManager::resumeIdle((CPU *) idleHardwarePlace);
+				}
+			}
+		}
+	}
+	
+	
 public:
 	//! \brief Performs the task dependency registration procedure
 	//! 
@@ -48,59 +109,15 @@ public:
 		assert(hardwarePlace != 0);
 		
 		DataAccess *dataAccess = finishedTask->popDataAccess();
-		std::deque<Task *> satifiedOriginators; // A temporary list of tasks to minimize the time spent with the mutex held. NOTE: This could be moved as a member of the WorkerThread for efficiency.
+		
+		// A temporary list of tasks to minimize the time spent with the mutex held.
+		satisfied_originator_list_t satisfiedOriginators; // NOTE: This could be moved as a member of the WorkerThread for efficiency.
 		while (dataAccess != 0) {
-			DataAccessSequence *dataAccessSequence = dataAccess->_dataAccessSequence;
-			DataAccessSequence::access_sequence_t::iterator dataAccessPosition = dataAccessSequence->_accessSequence.iterator_to(*dataAccess);
-			
-			// Erase the DataAccess and reevaluate if the following ones in the sequence become satisfied
-			// NOTE: This is done with lock held
-			{
-				std::lock_guard<SpinLock> guard(dataAccessSequence->_lock);
-				
-				Instrument::removedDataAccessFromSequence(
-					dataAccessSequence->_instrumentationId,
-					dataAccess->_instrumentationId,
-					finishedTask->getInstrumentationTaskId()
-				);
-				auto nextPosition = dataAccessSequence->_accessSequence.erase(dataAccessPosition);
-				
-				while (nextPosition != dataAccessSequence->_accessSequence.end()) {
-					bool becomesSatisfied = dataAccessSequence->reevaluateSatisfactibility(nextPosition);
-					if (becomesSatisfied) {
-						Instrument::dataAccessBecomesSatisfied(
-							dataAccessSequence->_instrumentationId,
-							nextPosition->_instrumentationId,
-							finishedTask->getInstrumentationTaskId(),
-							nextPosition->_originator->getInstrumentationTaskId()
-						);
-						satifiedOriginators.push_back(nextPosition->_originator);
-						
-						nextPosition++;
-					} else {
-						// Either it was already satisfied or it cannot become satisfied
-						break;
-					}
-				}
-			}
-			
-			// Process all the originators for whose a DataAccess has become satisfied
-			// NOTE: This is done without the lock held and may be slow since it can enter the scheduler
-			for (Task *satisfiedOriginator : satifiedOriginators) {
-				assert(satisfiedOriginator != 0);
-				
-				bool becomesReady = satisfiedOriginator->decreasePredecessors();
-				if (becomesReady) {
-					HardwarePlace *idleHardwarePlace = Scheduler::addReadyTask(satisfiedOriginator, hardwarePlace);
-					
-					if (idleHardwarePlace != nullptr) {
-						ThreadManager::resumeIdle((CPU *) idleHardwarePlace);
-					}
-				}
-			}
+			unregisterDataAccess(finishedTask->getInstrumentationTaskId(), dataAccess, /* OUT */ satisfiedOriginators);
+			processSatisfiedOriginators(satisfiedOriginators, hardwarePlace);
 			
 			dataAccess = finishedTask->popDataAccess();
-			satifiedOriginators.clear();
+			satisfiedOriginators.clear();
 		}
 	}
 };
