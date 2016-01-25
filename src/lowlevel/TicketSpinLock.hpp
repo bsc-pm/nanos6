1b12a3bfc854b01c9f29c4fe88a68166bdbfa5a0
Josep M. Perez <josep.m.perez@bsc.es>
2016-01-25 15:07:47 +0100

    Added implementation of a ticket spin lock based on C++11 atomics.

---
+++ src/lowlevel/TicketSpinLock.hpp
@@ -0,0 +1,43 @@
+#ifndef TICKET_SPIN_LOCK_HPP
+#define TICKET_SPIN_LOCK_HPP
+
+
+#include <atomic>
+#include <cassert>
+#include <cstddef>
+
+
+template <typename TICKET_T = uint16_t>
+class TicketSpinLock {
+private:
+	std::atomic<TICKET_T> _currentTicket;
+	std::atomic<TICKET_T> _nextFreeTicket;
+	
+public:
+	TicketSpinLock()
+		: _currentTicket(0), _nextFreeTicket(0)
+	{
+	}
+	
+	~TicketSpinLock()
+	{
+		// Not locked
+		assert(_currentTicket == _nextFreeTicket);
+	}
+	
+	inline void lock()
+	{
+		TICKET_T ticket = _nextFreeTicket++;
+		while (_currentTicket.load(std::memory_order_acquire)) {
+			// NOTE: there could be here some architecture-specific code to slow down the thread
+		}
+	}
+	
+	inline void unlock()
+	{
+		_currentTicket.fetch_add(1, std::memory_order_release);
+	}
+	
+};
+
+#endif // TICKET_SPIN_LOCK_HPP
