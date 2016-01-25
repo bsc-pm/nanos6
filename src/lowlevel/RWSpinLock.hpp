a29d34a763c836cd20ac469f009a2c9c81b2f6d2
Josep M. Perez <josep.m.perez@bsc.es>
2016-01-25 15:09:29 +0100

    Added a biased implementation of Read-Write spin locks based on the SpinLock class.

---
+++ src/lowlevel/RWSpinLock.hpp
@@ -0,0 +1,52 @@
+#ifndef RW_SPIN_LOCK_HPP
+#define RW_SPIN_LOCK_HPP
+
+
+
+#include "SpinLock.hpp"
+
+
+class RWSpinLock {
+private:
+	SpinLock _readersSpinLock;
+	long _readers;
+	SpinLock _writerSpinLock;
+	
+public:
+	RWSpinLock()
+		: _readersSpinLock(), _readers(0), _writerSpinLock()
+		{
+		}
+		
+	inline void readLock()
+	{
+		_readersSpinLock.lock();
+		if (++_readers == 1) {
+			_writerSpinLock.lock();
+		}
+		_readersSpinLock.unlock();
+	}
+	
+	inline void readUnlock()
+	{
+		_readersSpinLock.lock();
+		if (--_readers == 0) {
+			_writerSpinLock.unlock();
+		}
+		_readersSpinLock.unlock();
+	}
+	
+	inline void writeLock()
+	{
+		_writerSpinLock.lock();
+	}
+	
+	inline void writeUnlock()
+	{
+		_writerSpinLock.unlock();
+	}
+	
+};
+
+
+#endif // RW_SPIN_LOCK_HPP
