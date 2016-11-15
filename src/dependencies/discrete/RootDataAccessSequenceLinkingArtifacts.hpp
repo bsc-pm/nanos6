2b3bf4b2aaf310f4645248794b7359531229a881
Josep M. Perez <josep.m.perez@bsc.es>
2016-11-15 18:57:04 +0100

    Made the discrete dependency domain implementation fully intrusive.

---
+++ src/dependencies/discrete/RootDataAccessSequenceLinkingArtifacts.hpp
@@ -0,0 +1,32 @@
+#ifndef ROOT_DATA_ACCESS_SEQUENCE_LINKING_ARTIFACTS_HPP
+#define ROOT_DATA_ACCESS_SEQUENCE_LINKING_ARTIFACTS_HPP
+
+
+#include <boost/intrusive/avl_set_hook.hpp>
+
+
+struct RootDataAccessSequence;
+
+
+struct RootDataAccessSequenceLinkingArtifacts {
+	#if NDEBUG
+		typedef boost::intrusive::link_mode<boost::intrusive::normal_link> link_mode_t;
+	#else
+		typedef boost::intrusive::link_mode<boost::intrusive::safe_link> link_mode_t;
+	#endif
+	
+	typedef boost::intrusive::avl_set_member_hook<link_mode_t> hook_type;
+	typedef hook_type* hook_ptr;
+	typedef const hook_type* const_hook_ptr;
+	typedef RootDataAccessSequence value_type;
+	typedef value_type* pointer;
+	typedef const value_type* const_pointer;
+	
+	static inline constexpr hook_ptr to_hook_ptr (value_type &value);
+	static inline constexpr const_hook_ptr to_hook_ptr(const value_type &value);
+	static inline pointer to_value_ptr(hook_ptr n);
+	static inline const_pointer to_value_ptr(const_hook_ptr n);
+};
+
+
+#endif // ROOT_DATA_ACCESS_SEQUENCE_LINKING_ARTIFACTS_HPP
---
+++ src/dependencies/discrete/RootDataAccessSequenceLinkingArtifactsImplementation.hpp
@@ -0,0 +1,44 @@
+#ifndef ROOT_DATA_ACCESS_SEQUENCE_LINKING_ARTIFACTS_IMPLEMENTATION_HPP
+#define ROOT_DATA_ACCESS_SEQUENCE_LINKING_ARTIFACTS_IMPLEMENTATION_HPP
+
+
+#include <boost/intrusive/parent_from_member.hpp>
+
+#include "RootDataAccessSequence.hpp"
+#include "RootDataAccessSequenceLinkingArtifacts.hpp"
+
+
+inline constexpr RootDataAccessSequenceLinkingArtifacts::hook_ptr
+RootDataAccessSequenceLinkingArtifacts::to_hook_ptr (RootDataAccessSequenceLinkingArtifacts::value_type &value)
+{
+	return &value._rootDataAccessSequenceLinks;
+}
+
+inline constexpr RootDataAccessSequenceLinkingArtifacts::const_hook_ptr
+RootDataAccessSequenceLinkingArtifacts::to_hook_ptr(const RootDataAccessSequenceLinkingArtifacts::value_type &value)
+{
+	return &value._rootDataAccessSequenceLinks;
+}
+
+inline RootDataAccessSequenceLinkingArtifacts::pointer
+RootDataAccessSequenceLinkingArtifacts::to_value_ptr(RootDataAccessSequenceLinkingArtifacts::hook_ptr n)
+{
+	return (RootDataAccessSequenceLinkingArtifacts::pointer)
+		boost::intrusive::get_parent_from_member<RootDataAccessSequence>(
+			n,
+			&RootDataAccessSequence::_rootDataAccessSequenceLinks
+		);
+}
+
+inline RootDataAccessSequenceLinkingArtifacts::const_pointer
+RootDataAccessSequenceLinkingArtifacts::to_value_ptr(RootDataAccessSequenceLinkingArtifacts::const_hook_ptr n)
+{
+	return (RootDataAccessSequenceLinkingArtifacts::const_pointer)
+		boost::intrusive::get_parent_from_member<RootDataAccessSequence>(
+			n,
+			&RootDataAccessSequence::_rootDataAccessSequenceLinks
+		);
+}
+
+
+#endif // ROOT_DATA_ACCESS_SEQUENCE_LINKING_ARTIFACTS_IMPLEMENTATION_HPP
