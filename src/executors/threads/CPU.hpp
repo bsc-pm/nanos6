bsc-pm/nanos6

commit d6f33bd44b43551c947b3bcfbd82b3f5c5ffb834

Substitute pthread_setaffinity_np for sched_setaffinity

src/executors/threads/CPU.hpp
@@ -1,6 +1,9 @@
 #ifndef EXECUTORS_THREADS_CPU_HPP
 #define EXECUTORS_THREADS_CPU_HPP
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 
 #include "CPUDependencyData.hpp"
 
@@ -12,8 +15,10 @@
 #include <cassert>
 #include <deque>
 
+#include <unistd.h>
 #include <pthread.h>
 
+#include <sys/syscall.h>
 
 class WorkerThread;
 
@@ -66,7 +71,7 @@ struct CPU: public CPUPlace {
 	
 	inline void bindThread(pthread_t internalPThread)
 	{
-		int rc = pthread_setaffinity_np(internalPThread, CPU_ALLOC_SIZE(_systemCPUId+1), &_cpuMask);
+		int rc = sched_setaffinity(syscall(SYS_gettid), CPU_ALLOC_SIZE(_systemCPUId+1), &_cpuMask);
 		FatalErrorHandler::handle(rc, " when changing affinity of pthread ", internalPThread, " to CPU ", _systemCPUId);
 	}
 	

src/executors/threads/ThreadManager.cpp
@@ -1,8 +1,14 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <cassert>
 #include <list>
 
 #include <pthread.h>
 #include <unistd.h>
+
+#include <sys/syscall.h>
 
 #include "CPUActivation.hpp"
 #include "ThreadManager.hpp"
@@ -33,7 +39,7 @@ void ThreadManager::preinitialize()
 	_shutdownThreads = 0;
 	_mainShutdownControllerThread = nullptr;
 	
-	int rc = pthread_getaffinity_np(pthread_self(), sizeof(cpu_set_t), &_processCPUMask);
+	int rc = sched_getaffinity(syscall(SYS_gettid), sizeof(cpu_set_t), &_processCPUMask);
 	FatalErrorHandler::handle(rc, " when retrieving the affinity of the current pthread ", pthread_self());
 	
 	// Set up the pthread attributes for the threads of each CPU

src/executors/threads/WorkerThread.cpp
@@ -29,6 +29,8 @@ void *WorkerThread::body()
 {
 	ThreadManager::threadStartup(this);
 	
+	_cpu->bindThread(pthread_self());
+	
 	while (!_mustShutDown) {
 		if (!CPUActivation::acceptsWork(_cpu)) {
 			Scheduler::disableHardwarePlace(_cpu);

src/executors/threads/CPU.cpp
@@ -14,8 +14,5 @@ CPU::CPU(size_t systemCPUId, size_t virtualCPUId)
 	
 	int rc = pthread_attr_init(&_pthreadAttr);
 	FatalErrorHandler::handle(rc, " in call to pthread_attr_init");
-	
-	rc = pthread_attr_setaffinity_np(&_pthreadAttr, sizeof(cpu_set_t), &_cpuMask);
-	FatalErrorHandler::handle(rc, " when setting the affinity of a pthread in pthread_attr to ", systemCPUId);
 }
 

commit 01ca1d61faa30aa33094a7966fb5d01c85fd5e4c

Unverified attempt at fixing bug introduced in d6f33bd44b43551c947b3bcfbd82b3f5c5ffb834.

src/executors/threads/CPU.hpp
@@ -17,8 +17,8 @@
 
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/types.h>
 
-#include <sys/syscall.h>
 
 class WorkerThread;
 
@@ -69,10 +69,10 @@ struct CPU: public CPUPlace {
 	{
 	}
 	
-	inline void bindThread(pthread_t internalPThread)
+	inline void bindThread(pid_t tid)
 	{
-		int rc = sched_setaffinity(syscall(SYS_gettid), CPU_ALLOC_SIZE(_systemCPUId+1), &_cpuMask);
-		FatalErrorHandler::handle(rc, " when changing affinity of pthread ", internalPThread, " to CPU ", _systemCPUId);
+		int rc = sched_setaffinity(tid, CPU_ALLOC_SIZE(_systemCPUId+1), &_cpuMask);
+		FatalErrorHandler::handle(rc, " when changing affinity of pthread with thread id ", tid, " to CPU ", _systemCPUId);
 	}
 	
 };

src/executors/threads/Thread.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
 
 #include <pthread.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 #include "Thread.hpp"
 #include "lowlevel/FatalErrorHandler.hpp"
@@ -10,6 +12,8 @@ static void *thread_body_wrapper(void *parameter)
 {
 	Thread *thread = (Thread *) parameter;
 	assert(thread != nullptr);
+	thread->setTid(syscall(SYS_gettid));
+	
 	return thread->body();
 }
 

src/executors/threads/Thread.hpp
@@ -3,12 +3,15 @@
 
 
 #include <pthread.h>
+#include <sys/types.h>
 
 
 class Thread {
 protected:
 	//! The underlying pthread
 	pthread_t _pthread;
+	pid_t _tid;
+	
 	
 	inline void exit()
 	{
@@ -22,6 +25,12 @@ public:
 	
 	virtual ~Thread()
 	{
+	}
+	
+	// WARNING: This should be only called by the thread initialization code
+	inline void setTid(pid_t tid)
+	{
+		_tid = tid;
 	}
 	
 	void start(pthread_attr_t const *pthreadAttr);

src/executors/threads/ThreadManager.cpp
@@ -142,7 +142,7 @@ void ThreadManager::shutdown()
 				// Migrate the thread if necessary
 				idleThread->_cpuToBeResumedOn = cpu;
 				if (idleThread->_cpu != cpu) {
-					cpu->bindThread(idleThread->_pthread);
+					cpu->bindThread(idleThread->_tid);
 				}
 				
 				idleThread->signalShutdown();
@@ -210,7 +210,7 @@ void ThreadManager::threadShutdownSequence(WorkerThread *currentThread)
 				assert(next->_cpuToBeResumedOn == nullptr);
 				next->_cpuToBeResumedOn = cpu;
 				if (next->_cpu != cpu) {
-					cpu->bindThread(next->_pthread);
+					cpu->bindThread(next->_tid);
 				}
 				
 				// Resume the thread

src/executors/threads/ThreadManager.hpp
@@ -242,7 +242,7 @@ inline void ThreadManager::switchThreads(WorkerThread *currentThread, WorkerThre
 		assert(replacementThread->_cpuToBeResumedOn == nullptr);
 		replacementThread->_cpuToBeResumedOn = cpu;
 		if (replacementThread->_cpu != cpu) {
-			cpu->bindThread(replacementThread->_pthread);
+			cpu->bindThread(replacementThread->_tid);
 		}
 		
 		replacementThread->resume();
@@ -293,7 +293,7 @@ inline void ThreadManager::resumeThread(WorkerThread *suspendedThread, CPU *cpu,
 	assert(suspendedThread->_cpuToBeResumedOn == nullptr);
 	suspendedThread->_cpuToBeResumedOn = cpu;
 	if (suspendedThread->_cpu != cpu) {
-		cpu->bindThread(suspendedThread->_pthread);
+		cpu->bindThread(suspendedThread->_tid);
 	}
 	
 	if (!inInitializationOrShutdown) {
@@ -340,7 +340,7 @@ inline void ThreadManager::migrateThread(WorkerThread *currentThread, CPU *cpu)
 	
 	// Since it is the same thread the one that migrates itself, change the CPU directly
 	currentThread->_cpu = cpu;
-	cpu->bindThread(currentThread->_pthread);
+	cpu->bindThread(currentThread->_tid);
 }
 
 

src/executors/threads/WorkerThread.cpp
@@ -29,7 +29,7 @@ void *WorkerThread::body()
 {
 	ThreadManager::threadStartup(this);
 	
-	_cpu->bindThread(pthread_self());
+	_cpu->bindThread(_tid);
 	
 	while (!_mustShutDown) {
 		CPUActivation::activationCheck(this);