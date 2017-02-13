e348a14a157d8dea5465dee5e493458676f43bec
Josep M. Perez <josep.m.perez@bsc.es>
2017-02-13 12:33:11 +0100

    Added hardware counters infrastructure and an implementation using PAPI.

---
+++ src/performance/HardwareCounters.cpp
@@ -0,0 +1,19 @@
+#include "HardwareCounters.hpp"
+
+
+namespace HardwareCounters {
+	char const * const _presetCounterNames[total_preset_counter] = {
+		"Real frequency",
+		"Virtual frequency",
+		"IPC",
+		"L1 data cache miss ratio",
+		"L2 data cache miss ratio",
+		"L3 data cache miss ratio",
+		"GFLOP/s",
+		"Real usecs",
+		"Virtual usecs",
+		"Instructions"
+	};
+	
+};
+
---
+++ src/performance/HardwareCounters.hpp
@@ -0,0 +1,78 @@
+#ifndef HARDWARE_COUNTERS_HPP
+#define HARDWARE_COUNTERS_HPP
+
+
+#include <cstddef>
+#include <string>
+#include <utility>
+
+
+namespace HardwareCounters {
+	enum counter_types_t {
+		no_counters_counters_type = 0,
+		papi_counters_type
+	};
+	
+	enum preset_counter_t {
+		real_frequency_counter = 0,
+		virtual_frequency_counter,
+		ipc_counter,
+		l1_miss_ratio_counter,
+		l2_miss_ratio_counter,
+		l3_miss_ratio_count,
+		fpc_counter,
+		real_usecs_counter,
+		virtual_usecs_counter,
+		total_instructions,
+		total_preset_counter
+	};
+	
+	extern char const * const _presetCounterNames[total_preset_counter];
+	
+	
+	struct counter_value_t {
+		bool _isInteger;
+		std::string _name;
+		union {
+			long _integerValue;
+			double _floatValue;
+		};
+		std::string _units;
+		
+		counter_value_t(std::string const &name, double value, std::string const &units = "")
+			: _isInteger(false), _name(name), _floatValue(value), _units(units)
+		{
+		}
+		
+		counter_value_t(std::string const &name, long value, std::string const &units = "")
+			: _isInteger(true), _name(name), _integerValue(value), _units(units)
+		{
+		}
+	};
+	
+	
+	//! \brief A hardware counters set. For instance (1) a set for user code and (2) a set for the runtime code
+	template <int NUM_SETS = 1>
+	class Counters;
+	
+	template <int NUM_SETS = 1>
+	class ThreadCounters;
+	
+	
+	void initialize();
+	void shutdown();
+	
+	inline void initializeThread();
+	void shutdownThread();
+	
+}
+
+
+#if HAVE_PAPI
+#include "PAPI/PAPIHardwareCounters.hpp"
+#else
+#include "no-HC/NoHardwareCounters.hpp"
+#endif
+
+
+#endif // HARDWARE_COUNTERS_HPP
