01b0d38aa6ffc387ae60e600d82b83f267f299c5
Josep M. Perez <josep.m.perez@bsc.es>
2017-03-31 18:33:18 +0200

    Added support for the multidimensional API to the linear region dependency implementations.
    The support also includes checks to coalesce dimensions that are fully covered.

---
+++ src/dependencies/MultidimensionalAPITraversal.hpp
@@ -0,0 +1,61 @@
+#ifndef MULTIDIMENSIONAL_API_TRAVERSAL_HPP
+#define MULTIDIMENSIONAL_API_TRAVERSAL_HPP
+
+
+#include <cstddef>
+
+
+#ifdef NDEBUG
+#define _AI_ inline __attribute((always_inline))
+#else
+#define _AI_ inline
+#endif
+
+#define _UU_ __attribute__((unused))
+
+
+template <typename... TS>
+static _AI_ bool currentDimensionIsContinuous(long currentDimSize, long currentDimStart, long currentDimEnd, _UU_ TS... otherDimensions)
+{
+	return (currentDimStart == 0) && (currentDimEnd == currentDimSize);
+}
+
+
+template <typename... TS>
+static _AI_ long getCurrentDimensionSize(long currentDimSize, _UU_ long currentDimStart, _UU_ long currentDimEnd, _UU_ TS... otherDimensions)
+{
+	return currentDimSize;
+}
+
+template <typename... TS>
+static _AI_ long getCurrentDimensionStart(_UU_ long currentDimSize, long currentDimStart, _UU_ long currentDimEnd, _UU_ TS... otherDimensions)
+{
+	return currentDimStart;
+}
+
+template <typename... TS>
+static _AI_ long getCurrentDimensionEnd(_UU_ long currentDimSize, _UU_ long currentDimStart, long currentDimEnd, _UU_ TS... otherDimensions)
+{
+	return currentDimEnd;
+}
+
+
+template <typename... TS>
+static _AI_ size_t getStride()
+{
+	return 1;
+}
+
+
+template <typename... TS>
+static _AI_ size_t getStride(long currentDimSize, _UU_ long currentDimStart, _UU_ long currentDimEnd, TS... otherDimensions)
+{
+	return currentDimSize * getStride<>(otherDimensions...);
+}
+
+
+#undef _AI_
+#undef _UU_
+
+
+#endif // MULTIDIMENSIONAL_API_TRAVERSAL_HPP
