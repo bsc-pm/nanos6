cb358d36250e68498538619eeb93799f13b274e2
Josep M. Perez <josep.m.perez@bsc.es>
2017-04-26 17:03:56 +0200

    Fixed the format of boost assertion failure messages.

--- src/lowlevel/BoostAssertionFailureHandler.cpp
+++ src/lowlevel/BoostAssertionFailureHandler.cpp
@@ -7,12 +7,12 @@
 namespace boost {
 	void assertion_failed_msg(char const * expr, char const * msg, char const * function, char const * file, long line)
 	{
-		FatalErrorHandler::handle<>(1, file, ":", line, " ", function, " Boost assertion failure: ", msg, " when evaluating ", expr);
+		FatalErrorHandler::failIf<>(1, file, ":", line, " ", function, " Boost assertion failure: ", msg, " when evaluating ", expr);
 	}
 	
 	void assertion_failed(char const * expr, char const * function, char const * file, long line)
 	{
-		FatalErrorHandler::handle<>(1, file, ":", line, " ", function, " Boost assertion failure when evaluating ", expr);
+		FatalErrorHandler::failIf<>(1, file, ":", line, " ", function, " Boost assertion failure when evaluating ", expr);
 	}
 	
 }
