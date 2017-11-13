d861cf150f74d19b37135dec58cafe0685668b82
Josep M. Perez <josep.m.perez@bsc.es>
2017-11-13 15:32:42 +0100

    Removed deprecated headers.

--- api/nanos6/nanos6_debug_interface.h
+++
@@ -1,5 +0,0 @@
-/* This is a legacy header and will be removed. Include "nanos6/debug.h" instead. */
-
-#warning This is a legacy header and will be removed. Include "nanos6/debug.h" instead
-#include "debug.h"
-
--- api/nanos6/nanos6_rt_interface.h
+++
@@ -1,3 +0,0 @@
-/* This is a legacy header and will be removed. Instead Mercurium should include just "nanos6.h". */
-
-#include "../nanos6.h"
