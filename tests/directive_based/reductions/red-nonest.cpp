650121ab6dbf10a0ea0b291d58a20b34b7cc4dd6
Josep M. Perez <josep.m.perez@bsc.es>
2018-01-03 14:14:44 +0100

    Made one more test able to run with less tasks when configured with --enable-less-test-threads.
    This should avoid spurious errors in cpu-power.

--- tests/directive_based/reductions/red-nonest.cpp
+++ tests/directive_based/reductions/red-nonest.cpp
@@ -567,6 +567,11 @@ static VerifierConstraintCalculator _constraintCalculator;
 int main(int argc, char **argv)
 {
 	ncpus = nanos_get_num_cpus();
+	
+#if TEST_LESS_THREADS
+	ncpus = std::min(ncpus, 64);
+#endif
+	
 	delayMultiplier = sqrt(ncpus);
 	
 	int var1;
