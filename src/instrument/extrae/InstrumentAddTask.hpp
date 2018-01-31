bsc-pm/nanos6

commit facf5b22c53bc96522918c5a027c0ba1b3596184

Emit queue lenght an number of live tasks in extrae traces.

src/instrument/extrae/InstrumentAddTask.hpp
@@ -26,6 +26,8 @@ namespace Instrument {
 		__attribute__((unused)) size_t flags,
 		__attribute__((unused)) InstrumentationContext const &context
 	) {
+		size_t liveTasks = ++_liveTasks;
+		
 		extrae_combined_events_t ce;
 		
 		ce.HardwareCounters = 0;
@@ -34,11 +36,20 @@ namespace Instrument {
 		ce.nEvents = 1;
 		ce.nCommunications = 0;
 		
+		if (!_sampleTaskCount) {
+			ce.nEvents += 1;
+		}
+		
 		ce.Types  = (extrae_type_t *)  alloca (ce.nEvents * sizeof (extrae_type_t) );
 		ce.Values = (extrae_value_t *) alloca (ce.nEvents * sizeof (extrae_value_t));
 		
 		ce.Types[0] = _runtimeState;
 		ce.Values[0] = (extrae_value_t) NANOS_CREATION;
+		
+		if (!_sampleTaskCount) {
+			ce.Types[0] = _liveTasksEventType;
+			ce.Values[0] = (extrae_value_t) liveTasks;
+		}
 		
 		if (_traceAsThreads) {
 			_extraeThreadCountLock.readLock();

src/instrument/extrae/InstrumentExtrae.cpp
@@ -15,11 +15,15 @@ namespace Instrument {
 	const EnvironmentVariable<bool> _traceAsThreads("NANOS6_EXTRAE_AS_THREADS", 0);
 	const EnvironmentVariable<int> _sampleBacktraceDepth("NANOS6_EXTRAE_SAMPLE_BACKTRACE_DEPTH", 0);
 	const EnvironmentVariable<long> _sampleBacktracePeriod("NANOS6_EXTRAE_SAMPLE_BACKTRACE_PERIOD", 1000);
+	const EnvironmentVariable<bool> _sampleTaskCount("NANOS6_EXTRAE_SAMPLE_TASK_COUNT", 0);
 	
 	const extrae_type_t       _taskInstanceId = 9200002;
 	const extrae_type_t       _runtimeState = 9000000;     //!< Runtime state (extrae event type)
 	const extrae_type_t       _functionName = 9200011;     //!< Task function name
 	const extrae_type_t       _codeLocation = 9200021;     //!< Task code location
+	const extrae_type_t       _readyTasksEventType = 9200022;
+	const extrae_type_t       _liveTasksEventType = 9200023;
+	
 	const extrae_type_t       _nestingLevel = 9500001;     //!< Nesting level
 	const extrae_type_t       _samplingEventType = 30000000;
 	
@@ -36,6 +40,8 @@ namespace Instrument {
 	std::list<std::set<void *> *> _backtraceAddressSets;
 	
 	std::atomic<size_t> _nextTaskId(1);
+	std::atomic<size_t> _readyTasks(0);
+	std::atomic<size_t> _liveTasks(0);
 	
 	RWSpinLock _extraeThreadCountLock;
 	

src/instrument/extrae/InstrumentExtrae.hpp
@@ -35,11 +35,14 @@ namespace Instrument {
 	extern const EnvironmentVariable<bool>           _traceAsThreads;
 	extern const EnvironmentVariable<int>            _sampleBacktraceDepth;
 	extern const EnvironmentVariable<long>           _sampleBacktracePeriod;
+	extern const EnvironmentVariable<bool>           _sampleTaskCount;
 	
 	extern const extrae_type_t                       _taskInstanceId;
 	extern const extrae_type_t                       _runtimeState;      //!< Runtime state (extrae event type)
 	extern const extrae_type_t                       _functionName;      //!< Task function name
 	extern const extrae_type_t                       _codeLocation;      //!< Task function location
+	extern const extrae_type_t                       _readyTasksEventType;
+	extern const extrae_type_t                       _liveTasksEventType;
 	extern const extrae_type_t                       _nestingLevel;      //!< Task nesting level
 	extern const extrae_type_t                       _samplingEventType;
 	
@@ -57,6 +60,8 @@ namespace Instrument {
 	extern std::list<std::set<void *> *> _backtraceAddressSets;
 	
 	extern std::atomic<size_t> _nextTaskId;
+	extern std::atomic<size_t> _readyTasks;
+	extern std::atomic<size_t> _liveTasks;
 	
 	extern RWSpinLock _extraeThreadCountLock;
 	

src/instrument/extrae/InstrumentInitAndShutdown.cpp
@@ -208,6 +208,9 @@ namespace Instrument {
 		Extrae_define_event_type((extrae_type_t *) &_taskInstanceId, (char *) "Task instance", &zero, nullptr, nullptr);
 		Extrae_define_event_type((extrae_type_t *) &_nestingLevel, (char *) "Task nesting level", &zero, nullptr, nullptr);
 		
+		Extrae_define_event_type((extrae_type_t *) &_readyTasksEventType, (char *) "Number of ready tasks", &zero, nullptr, nullptr);
+		Extrae_define_event_type((extrae_type_t *) &_liveTasksEventType, (char *) "Number of live tasks", &zero, nullptr, nullptr);
+		
 		// Register the events for the backtrace
 		if (_sampleBacktraceDepth > 0) {
 			for (int i = 0; i < _sampleBacktraceDepth; i++) {

src/instrument/extrae/InstrumentLeaderThread.hpp
@@ -1,1 +1,50 @@
-../null/InstrumentLeaderThread.hpp
+/*
+	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
+	
+	Copyright (C) 2015-2018 Barcelona Supercomputing Center (BSC)
+*/
+
+#ifndef INSTRUMENT_EXTRAE_LEADER_THREAD_HPP
+#define INSTRUMENT_EXTRAE_LEADER_THREAD_HPP
+
+
+#include "../api/InstrumentLeaderThread.hpp"
+#include "InstrumentExtrae.hpp"
+
+
+namespace Instrument {
+	inline void leaderThreadSpin()
+	{
+		if (_sampleTaskCount) {
+			extrae_combined_events_t ce;
+			
+			ce.HardwareCounters = 0;
+			ce.Callers = 0;
+			ce.UserFunction = EXTRAE_USER_FUNCTION_NONE;
+			ce.nEvents = 2;
+			ce.nCommunications = 0;
+			
+			ce.Types  = (extrae_type_t *)  alloca (ce.nEvents * sizeof (extrae_type_t) );
+			ce.Values = (extrae_value_t *) alloca (ce.nEvents * sizeof (extrae_value_t));
+			
+			ce.Types[0] = _readyTasksEventType;
+			ce.Values[0] = (extrae_value_t) _readyTasks;
+			ce.Types[1] = _liveTasksEventType;
+			ce.Values[1] = (extrae_value_t) _liveTasks;
+			
+			if (_traceAsThreads) {
+				_extraeThreadCountLock.readLock();
+			}
+			
+			Extrae_emit_CombinedEvents ( &ce );
+			
+			if (_traceAsThreads) {
+				_extraeThreadCountLock.readUnlock();
+			}
+		}
+	}
+	
+}
+
+
+#endif // INSTRUMENT_EXTRAE_LEADER_THREAD_HPP

src/instrument/extrae/InstrumentTaskExecution.hpp
@@ -28,6 +28,10 @@ namespace Instrument {
 		ce.nEvents = 4;
 		ce.nCommunications = 0;
 		
+		if (!_sampleTaskCount) {
+			ce.nEvents += 1;
+		}
+		
 		ce.Types  = (extrae_type_t *)  alloca (ce.nEvents * sizeof (extrae_type_t) );
 		ce.Values = (extrae_value_t *) alloca (ce.nEvents * sizeof (extrae_value_t));
 		
@@ -42,6 +46,12 @@ namespace Instrument {
 		
 		ce.Types[3] = _taskInstanceId;
 		ce.Values[3] = (extrae_value_t) taskId._taskId;
+		
+		size_t readyTasks = --_readyTasks;
+		if (!_sampleTaskCount) {
+			ce.Types[4] = _readyTasksEventType;
+			ce.Values[4] = (extrae_value_t) readyTasks;
+		}
 		
 		ThreadLocalData &threadLocal = getThreadLocalData();
 		threadLocal._nestingLevels.push_back(taskId._nestingLevel);
@@ -68,6 +78,10 @@ namespace Instrument {
 		ce.nEvents = 4;
 		ce.nCommunications = 0;
 		
+		if (!_sampleTaskCount) {
+			ce.nEvents += 1;
+		}
+		
 		ce.Types  = (extrae_type_t *)  alloca (ce.nEvents * sizeof (extrae_type_t) );
 		ce.Values = (extrae_value_t *) alloca (ce.nEvents * sizeof (extrae_value_t));
 		
@@ -82,6 +96,12 @@ namespace Instrument {
 		
 		ce.Types[3] = _taskInstanceId;
 		ce.Values[3] = (extrae_value_t) taskId._taskId;
+		
+		size_t readyTasks = --_readyTasks;
+		if (!_sampleTaskCount) {
+			ce.Types[4] = _readyTasksEventType;
+			ce.Values[4] = (extrae_value_t) readyTasks;
+		}
 		
 		if (_traceAsThreads) {
 			_extraeThreadCountLock.readLock();
@@ -105,6 +125,10 @@ namespace Instrument {
 		ce.nEvents = 4;
 		ce.nCommunications = 0;
 		
+		if (!_sampleTaskCount) {
+			ce.nEvents += 1;
+		}
+		
 		ce.Types  = (extrae_type_t *)  alloca (ce.nEvents * sizeof (extrae_type_t) );
 		ce.Values = (extrae_value_t *) alloca (ce.nEvents * sizeof (extrae_value_t));
 		
@@ -119,6 +143,12 @@ namespace Instrument {
 		
 		ce.Types[3] = _taskInstanceId;
 		ce.Values[3] = (extrae_value_t) nullptr;
+		
+		size_t liveTasks = --_liveTasks;
+		if (!_sampleTaskCount) {
+			ce.Types[4] = _liveTasksEventType;
+			ce.Values[4] = (extrae_value_t) liveTasks;
+		}
 		
 		if (_traceAsThreads) {
 			_extraeThreadCountLock.readLock();

src/instrument/extrae/InstrumentTaskStatus.hpp
@@ -1,1 +1,79 @@
-../null/InstrumentTaskStatus.hpp
+/*
+	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
+	
+	Copyright (C) 2015-2018 Barcelona Supercomputing Center (BSC)
+*/
+
+#ifndef INSTRUMENT_EXTRAE_TASK_STATUS_HPP
+#define INSTRUMENT_EXTRAE_TASK_STATUS_HPP
+
+
+#include "../api/InstrumentTaskStatus.hpp"
+#include "InstrumentExtrae.hpp"
+
+
+namespace Instrument {
+	inline void taskIsPending(
+		__attribute__((unused)) task_id_t taskId,
+		__attribute__((unused)) InstrumentationContext const &context
+	) {
+	}
+	
+	inline void taskIsReady(
+		__attribute__((unused)) task_id_t taskId,
+		__attribute__((unused)) InstrumentationContext const &context
+	) {
+		if (_sampleTaskCount) {
+			return;
+		}
+		
+		extrae_combined_events_t ce;
+		
+		ce.HardwareCounters = 1;
+		ce.Callers = 0;
+		ce.UserFunction = EXTRAE_USER_FUNCTION_NONE;
+		ce.nEvents = 1;
+		ce.nCommunications = 0;
+		
+		if (!_sampleTaskCount) {
+			ce.nEvents += 1;
+		}
+		
+		ce.Types  = (extrae_type_t *)  alloca (ce.nEvents * sizeof (extrae_type_t) );
+		ce.Values = (extrae_value_t *) alloca (ce.nEvents * sizeof (extrae_value_t));
+		
+		ce.Types[0] = _readyTasksEventType;
+		ce.Values[0] = (extrae_value_t) ++_readyTasks;
+		
+		if (_traceAsThreads) {
+			_extraeThreadCountLock.readLock();
+		}
+		Extrae_emit_CombinedEvents ( &ce );
+		if (_traceAsThreads) {
+			_extraeThreadCountLock.readUnlock();
+		}
+	}
+	
+	inline void taskIsExecuting(
+		__attribute__((unused)) task_id_t taskId,
+		__attribute__((unused)) InstrumentationContext const &context
+	) {
+	}
+	
+	inline void taskIsBlocked(
+		__attribute__((unused)) task_id_t taskId,
+		__attribute__((unused)) task_blocking_reason_t reason,
+		__attribute__((unused)) InstrumentationContext const &context
+	) {
+	}
+	
+	inline void taskIsZombie(
+		__attribute__((unused)) task_id_t taskId,
+		__attribute__((unused)) InstrumentationContext const &context
+	) {
+	}
+	
+}
+
+
+#endif // INSTRUMENT_EXTRAE_TASK_STATUS_HPP

commit 789035a0a95729d1b166832403375e78d0d0f096

Fixed bug introduced in extrae instrumentation in commit facf5b22c53bc96522918c5a027c0ba1b3596184.

src/instrument/extrae/InstrumentAddTask.hpp
@@ -47,8 +47,8 @@ namespace Instrument {
 		ce.Values[0] = (extrae_value_t) NANOS_CREATION;
 		
 		if (!_sampleTaskCount) {
-			ce.Types[0] = _liveTasksEventType;
-			ce.Values[0] = (extrae_value_t) liveTasks;
+			ce.Types[1] = _liveTasksEventType;
+			ce.Values[1] = (extrae_value_t) liveTasks;
 		}
 		
 		if (_traceAsThreads) {