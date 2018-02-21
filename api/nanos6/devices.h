993851a5ef02a8cafd6262129dc1150c001ba423
Josep M. Perez <josep.m.perez@bsc.es>
2018-02-21 15:47:04 +0100

    Added devices header and CUDA devices header to support device environments.

---
+++ api/nanos6/devices.h
@@ -0,0 +1,19 @@
+/*
+	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
+	
+	Copyright (C) 2018 Barcelona Supercomputing Center (BSC)
+*/
+
+#ifndef NANOS6_DEVICES_H
+#define NANOS6_DEVICES_H
+
+#if NANOS6_CUDA
+#include "cuda_device.h"
+#endif
+
+#if NANOS6_OPENCL
+#include "opencl_device.h"
+#endif
+
+#endif /* NANOS6_DEVICES_H */
+
