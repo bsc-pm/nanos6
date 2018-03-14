04cf5f7acb74b484afe818e31c6384fac15f6fb4
Marcos Maroñas <marcos.maronasbravo@bsc.es>
2018-03-14 14:48:47 +0100

    Added support for hwloc2.

--- src/hardware/HardwareInfo.cpp
+++ src/hardware/HardwareInfo.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "HardwareInfo.hpp"
+#include "lowlevel/FatalErrorHandler.hpp"
 #include <hwloc.h>
 #include <assert.h>
 
@@ -23,6 +24,12 @@ std::vector<ComputePlace*> HardwareInfo::_computeNodes;
 
 void HardwareInfo::initialize()
 {
+//! Check that hwloc headers match with runtime.
+#if HWLOC_API_VERSION >= 0x00020000 
+	FatalErrorHandler::failIf(hwloc_get_api_version() < 0x20000, "hwloc headers are more recent than runtime library.");
+#else
+	FatalErrorHandler::failIf(hwloc_get_api_version() >= 0x20000, "hwloc headers are older than runtime library.");
+#endif
 	//! Hardware discovery
 	hwloc_topology_t topology;
 	hwloc_topology_init(&topology);  // initialization
@@ -44,10 +51,12 @@ void HardwareInfo::initialize()
 			//! Create a MemoryPlace for each NUMA node.
 			//! Get the hwloc obj representing the NUMA node. 
 			hwloc_obj_t obj = hwloc_get_obj_by_type(topology, HWLOC_NUMA_ALIAS, i);
+			assert(obj != nullptr);
 			//! Create the MemoryPlace representing the NUMA node with its index and AddressSpace. 
 			node = new NUMAPlace(obj->logical_index, NUMAAddressSpace);
 			//! Add the MemoryPlace to the list of memory nodes of the HardwareInfo.
 			_memoryNodes[node->getIndex()] = node;
+			node = nullptr;
 		}
 	} 
 	else {
@@ -66,7 +75,35 @@ void HardwareInfo::initialize()
 	_computeNodes.resize(coresCount);
 	for(int i = 0; i < coresCount; i++) {
 		hwloc_obj_t obj = hwloc_get_obj_by_type(topology, HWLOC_OBJ_PU, i);
+		assert(obj != nullptr);
+#if HWLOC_API_VERSION >= 0x00020000
+		hwloc_obj_t ancestor = nullptr; 
+		hwloc_obj_t nodeNUMA = nullptr;
+
+		//! NUMA node can be found in different depths of ancestors (ordered from deeper to narrower): 
+		//! 1. A L3CACHE object / A GROUP object.
+		//! 2. The most common is a PACKAGE object.
+		//! 3. The MACHINE object.
+		//! ref: https://www.open-mpi.org/projects/hwloc/doc/v2.0.0/a00327.php
+		ancestor = hwloc_get_ancestor_obj_by_type(topology, HWLOC_OBJ_L3CACHE, obj);
+		if(ancestor == nullptr || ancestor->memory_arity != 1) {
+			ancestor = hwloc_get_ancestor_obj_by_type(topology, HWLOC_OBJ_GROUP, obj);
+			if(ancestor == nullptr || ancestor->memory_arity != 1) {
+				ancestor = hwloc_get_ancestor_obj_by_type(topology, HWLOC_OBJ_PACKAGE, obj);
+				if(ancestor == nullptr || ancestor->memory_arity != 1) {
+					ancestor = hwloc_get_ancestor_obj_by_type(topology, HWLOC_OBJ_MACHINE, obj);
+				}
+			}
+		}
+		assert(ancestor != nullptr);
+		assert(ancestor->memory_arity == 1);
+
+		nodeNUMA = ancestor->memory_first_child;
+		assert(nodeNUMA != nullptr);
+		assert(hwloc_obj_type_is_memory(nodeNUMA->type));
+#else
 		hwloc_obj_t nodeNUMA = hwloc_get_ancestor_obj_by_type(topology, HWLOC_NUMA_ALIAS, obj);
+#endif
 		size_t NUMANodeId = nodeNUMA == NULL ? 0 : nodeNUMA->logical_index;
 		CPU * cpu = new CPU( /*systemCPUID*/ obj->os_index, /*virtualCPUID*/ obj->logical_index, NUMANodeId);
 		_computeNodes[obj->logical_index] = cpu;
