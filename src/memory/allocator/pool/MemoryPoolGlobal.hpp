bsc-pm/nanos6

commit 1b262170a8f0344c63f90e90459e3f12839ebf0f

Update chunk size when requesting big chunk sizes

src/memory/allocator/pool/MemoryPoolGlobal.hpp
@@ -75,7 +75,7 @@ public:
 		}
 	}
 	
-	void *getMemory(size_t &chunkSize)
+	void *getMemory(size_t minSize, size_t &chunkSize)
 	{
 		std::lock_guard<SpinLock> guard(_lock);
 		if (_curAvailable == 0) {
@@ -85,6 +85,13 @@ public:
 		void *curAddr = _curMemoryChunk;
 		
 		chunkSize = _memoryChunkSize;
+		
+		if (chunkSize < minSize) {
+			// Get minimum acceptable chunkSize
+			chunkSize = ((minSize + _memoryChunkSize - 1) / _memoryChunkSize) * _memoryChunkSize;
+			_memoryChunkSize.setValue(chunkSize);
+		}
+		
 		_curAvailable -= chunkSize;
 		_curMemoryChunk = (char *)_curMemoryChunk + chunkSize;
 		

src/memory/allocator/pool/MemoryPool.hpp
@@ -22,7 +22,7 @@ private:
 	void fillPool()
 	{
 		size_t globalChunkSize;
-		_topChunk = _globalAllocator->getMemory(globalChunkSize);
+		_topChunk = _globalAllocator->getMemory(_chunkSize, globalChunkSize);
 		
 		// If globalChunkSize % _chunkSize != 0, some memory will be left unused
 		size_t numChunks = globalChunkSize / _chunkSize;

commit 8b63dfd7368465744f7ccaa581e79c8f38475265

Correct bug introduced in commit 1b26217

src/memory/allocator/pool/MemoryPoolGlobal.hpp
@@ -78,7 +78,12 @@ public:
 	void *getMemory(size_t minSize, size_t &chunkSize)
 	{
 		std::lock_guard<SpinLock> guard(_lock);
-		if (_curAvailable == 0) {
+		if (_curAvailable < _memoryChunkSize) {
+			if (_curAvailable != 0) {
+				// Chunk size was changed previously, update also alloc size to make all sizes fit again
+				_globalAllocSize.setValue(((_globalAllocSize + _memoryChunkSize - 1) / _memoryChunkSize) * _memoryChunkSize);
+			}
+			
 			fillPool();
 		}
 		
@@ -90,6 +95,11 @@ public:
 			// Get minimum acceptable chunkSize
 			chunkSize = ((minSize + _memoryChunkSize - 1) / _memoryChunkSize) * _memoryChunkSize;
 			_memoryChunkSize.setValue(chunkSize);
+			
+			if (_curAvailable < chunkSize) {
+				_globalAllocSize.setValue(((_globalAllocSize + _memoryChunkSize - 1) / _memoryChunkSize) * _memoryChunkSize);
+				fillPool();
+			}
 		}
 		
 		_curAvailable -= chunkSize;