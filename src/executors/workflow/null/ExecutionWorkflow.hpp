ee512b60698e1a141dd0f9378d14f9b2e122bbed
Kevin Sala <kevin.sala@bsc.es>
2019-06-17 18:28:43 +0200

    Adding missing copyrights in source code files.

--- src/executors/workflow/null/ExecutionWorkflow.hpp
+++ src/executors/workflow/null/ExecutionWorkflow.hpp
@@ -1,3 +1,9 @@
+/*
+	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
+	
+	Copyright (C) 2019 Barcelona Supercomputing Center (BSC)
+*/
+
 #ifndef EXECUTION_WORKFLOW_HPP
 #define EXECUTION_WORKFLOW_HPP
 
