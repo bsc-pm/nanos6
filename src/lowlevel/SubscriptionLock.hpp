f8610800ec889c6c26e481465c3a6c67209c9557
Antoni Navarro <anavarr1@bsc.es>
2019-11-13 10:40:53 +0100

    Fix styling and tweak the SubscriptionLock

--- src/lowlevel/SubscriptionLock.hpp
+++ src/lowlevel/SubscriptionLock.hpp
@@ -1,10 +1,8 @@
 /*
 	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
-	
-	Copyright (C) 2015-2019 Barcelona Supercomputing Center (BSC)
+
+	Copyright (C) 2019 Barcelona Supercomputing Center (BSC)
 */
-
-
 
 #ifndef SUBSCRIPTION_LOCK_HPP
 #define SUBSCRIPTION_LOCK_HPP
@@ -21,93 +19,119 @@
 
 
 class SubscriptionLock {
-	struct node {
-		std::atomic<uint64_t> ticket;
-		std::atomic<uint64_t> cpuIdx;
+
+	struct Node {
+		std::atomic<uint64_t> _ticket;
+		std::atomic<uint64_t> _cpuId;
 	};
-	
+
 protected:
-	alignas(CACHELINE_SIZE) Padded<node> *_waitq;
+
+	alignas(CACHELINE_SIZE) Padded<Node> *_waitQueue;
 	alignas(CACHELINE_SIZE) std::atomic<uint64_t> _head;
 	alignas(CACHELINE_SIZE) uint64_t _next;
 	alignas(CACHELINE_SIZE) uint64_t _size;
-	
-	inline void pushWaitingCPU(uint64_t const ticket, uint64_t const cpuIndex)
-	{
-		_waitq[ticket % _size].cpuIdx.store(ticket+cpuIndex, std::memory_order_relaxed);
-	}
-	
+
 public:
+
+	//! \brief SubscriptionLock's constructor
+	//!
+	//! \param[in] size The number of slots in the subscription lock, i.e., the
+	//! maximum number of threads that can access this lock simultaneously
 	SubscriptionLock(size_t size)
 		: _head(size), _next(size+1), _size(size)
 	{
-		_waitq = (Padded<node> *) MemoryAllocator::alloc(size * sizeof(Padded<node>));
+		_waitQueue = (Padded<Node> *) MemoryAllocator::alloc(size * sizeof(Padded<Node>));
 		for (size_t i = 0; i < size; i++) {
-			new (&_waitq[i]) Padded<node>();
+			new (&_waitQueue[i]) Padded<Node>();
 		}
-		
-		_waitq[0].ticket = _size;
-		_waitq[0].cpuIdx = 0;
+
+		_waitQueue[0]._ticket = _size;
+		_waitQueue[0]._cpuId = 0;
 	}
-	
+
 	~SubscriptionLock()
 	{
-		MemoryAllocator::free(_waitq, _size * sizeof(Padded<node>));
+		MemoryAllocator::free(_waitQueue, _size * sizeof(Padded<Node>));
 	}
-	
-	inline bool popWaitingCPU(uint64_t const myTicket, uint64_t & cpu)
+
+	//! \brief Try to get a currently subscribed CPU
+	//!
+	//! \param[in] myTicket The ticket related to the subscriber (if it exists)
+	//! \param[out] cpuId Upon returning true, this parameter contains the id
+	//! of the subscriber CPU we've just popped from the waiting queue
+	//!
+	//! \return Whether a CPU was popped from the waiting queue
+	inline bool popWaitingCPU(uint64_t const myTicket, uint64_t &cpuId)
 	{
-		uint64_t const cpuIdx = _waitq[myTicket % _size].cpuIdx.load(std::memory_order_relaxed);
-		cpu = cpuIdx - myTicket;
-		return  cpuIdx > myTicket;
+		uint64_t const innerCPUId = _waitQueue[myTicket % _size]._cpuId.load(std::memory_order_relaxed);
+		cpuId = innerCPUId - myTicket;
+		return innerCPUId > myTicket;
 	}
-	
+
+	//! \brief Obtain the lock without subscribing
 	inline void lock()
 	{
 		const uint64_t head = _head.fetch_add(1, std::memory_order_relaxed);
-		const uint64_t idx = head % _size;
-		while (_waitq[idx].ticket.load(std::memory_order_acquire) != head) {
+		const uint64_t id = head % _size;
+
+		// Wait until it's our turn
+		while (_waitQueue[id]._ticket.load(std::memory_order_acquire) != head) {
 			spinWait();
 		}
 	}
-	
+
+	//! \brief Subscribe or obtain the lock
+	//!
+	//! \param[in] cpuIndex The index of the CPU
+	//!
+	//! \return The ticket obtained for the current subscription
 	inline uint64_t subscribeOrLock(uint64_t const cpuIndex)
 	{
 		const uint64_t head = _head.fetch_add(1, std::memory_order_relaxed);
-		pushWaitingCPU(head, cpuIndex);
-		const uint64_t idx = head % _size;
-		while (_waitq[idx].ticket.load(std::memory_order_acquire) < head) {
+		const uint64_t id = head % _size;
+		_waitQueue[id]._cpuId.store(head+cpuIndex, std::memory_order_relaxed);
+
+		// Wait until it's our turn or someone else has served our subscription
+		while (_waitQueue[id]._ticket.load(std::memory_order_acquire) < head) {
 			spinWait();
 		}
+
 		return head;
 	}
-	
+
+	//! \brief Try to obtain the lock
+	//!
+	//! \return Whether the lock was obtained
 	inline bool tryLock()
 	{
 		uint64_t head = _head.load(std::memory_order_relaxed);
-		const uint64_t idx = head % _size;
-		if (_waitq[idx].ticket.load(std::memory_order_relaxed) != head) {
+		const uint64_t id = head % _size;
+		if (_waitQueue[id]._ticket.load(std::memory_order_relaxed) != head) {
 			return false;
 		}
+
 		return std::atomic_compare_exchange_weak_explicit(
-				&_head,
-				&head,
-				head+1,
-				std::memory_order_acquire,
-				std::memory_order_relaxed);
+			&_head,
+			&head,
+			head+1,
+			std::memory_order_acquire,
+			std::memory_order_relaxed
+		);
 	}
-	
+
+	//! \brief Unsubscribe from the lock (advance the ticket)
 	inline void unsubscribe()
 	{
-		const uint64_t idx = _next % _size;
-		_waitq[idx].ticket.store(_next++, std::memory_order_release);
+		const uint64_t id = _next % _size;
+		_waitQueue[id]._ticket.store(_next++, std::memory_order_release);
 	}
-	
+
 	inline uint64_t waiting()
 	{
 		return _head.load(std::memory_order_relaxed) - (_next);
 	}
 };
 
-#endif //SUBSCRIPTION_LOCK_HPP
+#endif // SUBSCRIPTION_LOCK_HPP
 
