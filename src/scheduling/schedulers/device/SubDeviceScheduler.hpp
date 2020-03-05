5af36b86e68d282dab769ae6bbd25a32d2e8a11c
Kevin Sala <kevin.sala@bsc.es>
2020-03-05 19:54:07 +0100

    Fixing SubDeviceScheduler's getName function.
    Issue was reported by @dalvare1.

--- src/scheduling/schedulers/device/SubDeviceScheduler.hpp
+++ src/scheduling/schedulers/device/SubDeviceScheduler.hpp
@@ -1,11 +1,13 @@
 /*
 	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
-	
-	Copyright (C) 2019 Barcelona Supercomputing Center (BSC)
+
+	Copyright (C) 2019-2020 Barcelona Supercomputing Center (BSC)
 */
 
 #ifndef SUB_DEVICE_SCHEDULER_HPP
 #define SUB_DEVICE_SCHEDULER_HPP
+
+#include <string>
 
 #include "DeviceScheduler.hpp"
 #include "hardware/places/DeviceComputePlace.hpp"
@@ -15,7 +17,7 @@ class SubDeviceScheduler : public DeviceScheduler {
 	size_t _totalSubDevices;
 	int _deviceSubType;
 	std::vector<ComputePlace *> _cpuToSubDevice;
-	
+
 public:
 	SubDeviceScheduler(SchedulingPolicy policy, bool enablePriority, bool enableImmediateSuccessor, nanos6_device_t deviceType, int deviceSubType) :
 		DeviceScheduler(policy, enablePriority, enableImmediateSuccessor, deviceType),
@@ -24,47 +26,47 @@ public:
 	{
 		DeviceInfoImplementation *deviceInfo = static_cast<DeviceInfoImplementation*>(HardwareInfo::getDeviceInfo(deviceType));
 		assert(deviceInfo != nullptr);
-		
+
 		Device *subDeviceType = deviceInfo->getDevice(_deviceSubType);
 		assert(subDeviceType != nullptr);
-		
+
 		_totalSubDevices = subDeviceType->getNumDevices();
 	}
-	
+
 	virtual ~SubDeviceScheduler()
 	{}
-	
+
 	inline int getDeviceSubType()
 	{
 		return _deviceSubType;
 	}
-	
+
 	Task *getReadyTask(ComputePlace *computePlace, ComputePlace *deviceComputePlace)
 	{
 		assert(deviceComputePlace != nullptr);
 		assert(((DeviceComputePlace *)deviceComputePlace)->getSubType() == _deviceSubType);
-		
+
 		Task *result = getTask(computePlace, deviceComputePlace);
 		assert(result == nullptr || result->getDeviceSubType() == _deviceSubType);
 		return result;
 	}
-	
+
 	inline std::string getName() const
 	{
-		return "SubDeviceScheduler" + _deviceSubType;
+		return "SubDeviceScheduler(" + std::to_string(_deviceSubType) + ")";
 	}
-	
+
 protected:
 	inline ComputePlace *getRelatedComputePlace(uint64_t cpuIndex) const
 	{
 		return _cpuToSubDevice[cpuIndex];
 	}
-	
+
 	inline void setRelatedComputePlace(uint64_t cpuIndex, ComputePlace *computePlace)
 	{
 		DeviceComputePlace *deviceComputePlace = (DeviceComputePlace *)computePlace;
 		assert(deviceComputePlace == nullptr || deviceComputePlace->getSubType() == _deviceSubType);
-		
+
 		_cpuToSubDevice[cpuIndex] = deviceComputePlace;
 	}
 };
