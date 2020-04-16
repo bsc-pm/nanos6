aade785cd7bec20e3b96b3b39f21e3136f483c12
Antoni Navarro <anavarr1@bsc.es>
2020-04-16 15:21:14 +0200

    Relocate TaskInfo files

--- src/system/TaskInfoAPI.cpp
+++ src/system/TaskInfoAPI.cpp
@@ -6,7 +6,7 @@
 
 #include <nanos6/task-info-registration.h>
 
-#include "ompss/TaskInfo.hpp"
+#include "tasks/TaskInfo.hpp"
 
 
 extern "C" void nanos6_register_task_info(nanos6_task_info_t *task_info)
