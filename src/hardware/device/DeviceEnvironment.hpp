b160ad2d1f816a287e92871da1f89907da78104b
Ruben Cano Diaz <ruben.canodiaz@bsc.es>
2020-05-22 22:50:47 +0300

    Extended Task to include the device environment.
    Using the void *_deviceData to pass device specific information
    would lead to more malloc/free calls per device task with potential
    performance penalty.
    
    It is now used preallocated within the Task object itself, as a union,
    to occupy only the space needed by the most demanding, configuration-
    enabled device.

---
+++ src/hardware/device/DeviceEnvironment.hpp
@@ -0,0 +1,27 @@
+/*
+	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
+
+	Copyright (C) 2020 Barcelona Supercomputing Center (BSC)
+*/
+
+#ifndef DEVICE_ENVIRONMENT_HPP
+#define DEVICE_ENVIRONMENT_HPP
+
+// Use this header to define the device environment -used by Mercurium- for various devices
+// implementations.
+
+#include <config.h>
+
+#if USE_CUDA
+#include <nanos6/cuda_device.h>
+#endif
+
+//! Contains the device environment based on configured device types to determine max size
+union DeviceEnvironment {
+#if USE_CUDA
+		nanos6_cuda_device_environment_t cuda;
+#endif
+	};
+
+#endif // DEVICE_ENVIRONMENT_HPP
+
