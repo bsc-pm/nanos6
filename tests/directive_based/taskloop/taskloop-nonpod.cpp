70a3ad0ce2002d3255f184b5cfb05b776185a2ee
Marcos Maroñas <marcos.maronasbravo@bsc.es>
2020-05-29 20:48:27 +0200

    Adding tests to check the issue of nonpod args in taskloop and taskfor.

---
+++ tests/directive_based/taskloop/taskloop-nonpod.cpp
@@ -0,0 +1,32 @@
+/*
+	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
+
+	Copyright (C) 2020 Barcelona Supercomputing Center (BSC)
+*/
+
+#include <algorithm>
+#include <cstdlib>
+#include <vector>
+
+#include "TestAnyProtocolProducer.hpp"
+
+#define N 100
+
+TestAnyProtocolProducer tap;
+
+int main() {
+	std::vector<int> v(N);
+	int i;
+
+	tap.registerNewTests(1);
+	tap.begin();
+
+	#pragma oss taskloop firstprivate(v)
+	for (i = 0; i < N; ++i)
+	{}
+	#pragma oss taskwait
+
+	tap.evaluate(true, "Program finished correctly");
+	tap.end();
+	return 0;
+}
