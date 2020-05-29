373adfb19afc6394e1d6f8219dbe2834016634d1
Kevin Sala <kevin.sala@bsc.es>
2020-05-29 21:36:57 +0200

    Improving a bit nonpod taskfor and taskloop tests.

--- tests/directive_based/task-for/task-for-nonpod.cpp
+++ tests/directive_based/task-for/task-for-nonpod.cpp
@@ -15,18 +15,25 @@
 TestAnyProtocolProducer tap;
 
 int main() {
-	std::vector<int> v(N);
+	std::vector<int> v(N, N);
 	int i;
 
 	tap.registerNewTests(1);
 	tap.begin();
 
 	#pragma oss task for firstprivate(v)
-	for (i = 0; i < N; ++i)
-	{}
+	for (i = 0; i < N; ++i) {
+		++v[i];
+	}
 	#pragma oss taskwait
 
-	tap.evaluate(true, "Program finished correctly");
+	bool correct = true;
+	for (i = 0; i < N; ++i) {
+		if (v[i] != N)
+			correct = false;
+	}
+
+	tap.evaluate(correct, "Program finished correctly");
 	tap.end();
 	return 0;
 }
