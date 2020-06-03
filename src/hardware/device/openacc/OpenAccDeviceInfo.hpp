fe23b5388c60fb2330c64a59e667250a6ac3b3ab
Orestis Korakitis <orestis.korakitis@bsc.es>
2020-06-03 15:57:05 +0300

    OpenACC: Introduced OpenACC device info class

---
+++ src/hardware/device/openacc/OpenAccDeviceInfo.hpp
@@ -0,0 +1,68 @@
+/*
+	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
+
+	Copyright (C) 2020 Barcelona Supercomputing Center (BSC)
+*/
+
+#ifndef OPENACC_DEVICE_INFO_HPP
+#define OPENACC_DEVICE_INFO_HPP
+
+#include <hardware/hwinfo/DeviceInfo.hpp>
+#include <hardware/places/ComputePlace.hpp>
+#include <hardware/places/MemoryPlace.hpp>
+
+#include "OpenAccAccelerator.hpp"
+#include "OpenAccFunctions.hpp"
+
+class OpenAccDeviceInfo : public DeviceInfo {
+	std::vector<OpenAccAccelerator *> _accelerators;
+
+public:
+	OpenAccDeviceInfo()
+	{
+		_deviceCount = OpenAccFunctions::getDeviceCount();
+		_accelerators.reserve(_deviceCount);
+
+		if (_deviceCount > 0) {
+			// Create an Accelerator instance for each physical device
+			for (size_t i = 0; i < _deviceCount; ++i) {
+				OpenAccAccelerator *accelerator = new OpenAccAccelerator(i);
+				assert(accelerator != nullptr);
+				_accelerators.push_back(accelerator);
+			}
+
+			_deviceInitialized = true;
+		}
+	}
+
+	~OpenAccDeviceInfo()
+	{
+		for (OpenAccAccelerator *accelerator : _accelerators) {
+			assert(accelerator != nullptr);
+			delete accelerator;
+		}
+	}
+
+	inline size_t getComputePlaceCount() const
+	{
+		return _deviceCount;
+	}
+
+	ComputePlace *getComputePlace(int handler)
+	{
+		return _accelerators[handler]->getComputePlace();
+	}
+
+	inline size_t getMemoryPlaceCount() const
+	{
+		return _deviceCount;
+	}
+
+	MemoryPlace *getMemoryPlace(int handler)
+	{
+		return _accelerators[handler]->getMemoryPlace();
+	}
+};
+
+#endif // OPENACC_DEVICE_INFO_HPP
+
