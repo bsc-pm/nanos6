df692865fe488e8287210fd63aba5741211d7282
Orestis Korakitis <orestis.korakitis@bsc.es>
2020-06-04 21:20:24 +0300

    OpenACC: Moved all OpenAccQueuePool code to header, with various minor fixes

--- src/hardware/device/openacc/OpenAccQueuePool.hpp
+++ src/hardware/device/openacc/OpenAccQueuePool.hpp
@@ -4,9 +4,11 @@
 	Copyright (C) 2020 Barcelona Supercomputing Center (BSC)
 */
 
-#ifndef OPENACC_QUEUE_HPP
-#define OPENACC_QUEUE_HPP
+#ifndef OPENACC_QUEUE_POOL_HPP
+#define OPENACC_QUEUE_POOL_HPP
 
+#include "OpenAccFunctions.hpp"
+#include "lowlevel/FatalErrorHandler.hpp"
 #include "tasks/Task.hpp"
 
 class OpenAccQueue {
@@ -16,7 +18,8 @@ private:
 
 public:
 	OpenAccQueue(int id) :
-		_queueId(id)
+		_queueId(id),
+		_task(nullptr)
 	{
 	}
 
@@ -24,17 +27,17 @@ public:
 	{
 	}
 
-	inline int getQueueId()
+	inline int getQueueId() const
 	{
 		return _queueId;
 	}
 
-	inline bool isFinished()
+	inline bool isFinished() const
 	{
 		return OpenAccFunctions::asyncFinished(_queueId);
 	}
 
-	inline Task *getTask()
+	inline Task *getTask() const
 	{
 		return _task;
 	}
@@ -57,15 +60,37 @@ private:
 	size_t _maxAsyncQueues;
 
 public:
-	OpenAccQueuePool();
+	OpenAccQueuePool() :
+		_maxAsyncQueues(OpenAccFunctions::getMaxQueues())
+	{
+		// preallocate the default num of queues; on request expand up to _maxAsyncQueues
+		size_t numQueues = OpenAccFunctions::getInitialQueueNum();
+		FatalErrorHandler::failIf(numQueues > _maxAsyncQueues,
+			"NANOS6_OPENACC_DEFAULT_QUEUES can't be greater than NANOS6_OPENACC_MAX_QUEUES",
+			"\nPlease set environment variables accordingly");
 
-	~OpenAccQueuePool();
+		for (size_t i = 1; i <= numQueues; i++) {	// count from 1; keep 0 for special cases (eg. in_final)
+			OpenAccQueue *queue = new OpenAccQueue((int)i);
+			assert(queue != nullptr);
+			_queuePool.push_back(queue);
+		}
+		_nextAsyncId = numQueues + 1;
+	}
+
+	~OpenAccQueuePool()
+	{
+		while (!_queuePool.empty()) {
+			OpenAccQueue *queue = _queuePool.front();
+			_queuePool.pop_front();
+			delete queue;
+		}
+	}
 
 	// We have available queues in 2 cases:
 	//   1. The existing queues pool is not empty
 	//   2. We haven't reached the defined MAX queue number, so we can allocate more
 	//      (see NANOS6_OPENACC_MAX_QUEUES)
-	inline bool isQueueAvailable()
+	inline bool isQueueAvailable() const
 	{
 		if (_queuePool.empty()) {
 			if (_nextAsyncId > _maxAsyncQueues) {
@@ -75,7 +100,17 @@ public:
 		return true;
 	}
 
-	OpenAccQueue *getAsyncQueue();
+	inline OpenAccQueue *getAsyncQueue()
+	{
+		if (_queuePool.empty()) {
+			assert(_nextAsyncId <= _maxAsyncQueues);
+			return new OpenAccQueue(_nextAsyncId++);
+		} else {
+			OpenAccQueue *queue = _queuePool.front();
+			_queuePool.pop_front();
+			return queue;
+		}
+	}
 
 	inline void releaseAsyncQueue(OpenAccQueue *queue)
 	{
@@ -84,4 +119,4 @@ public:
 
 };
 
-#endif // OPENACC_QUEUE_HPP
+#endif // OPENACC_QUEUE_POOL_HPP
