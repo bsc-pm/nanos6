8a1fa52414ef2dfe3d23057727a3544c77bc187f
Orestis Korakitis <orestis.korakitis@bsc.es>
2020-06-09 21:46:31 +0300

    Re-enabled if0 task handling in Accelerators

--- src/hardware/device/Accelerator.cpp
+++ src/hardware/device/Accelerator.cpp
@@ -8,6 +8,7 @@
 
 #include "executors/threads/TaskFinalization.hpp"
 #include "hardware/HardwareInfo.hpp"
+#include "scheduling/Scheduler.hpp"
 #include "tasks/TaskImplementation.hpp"
 
 #include <DataAccessRegistration.hpp>
@@ -39,6 +40,17 @@ void Accelerator::finishTask(Task *task)
 
 	CPUDependencyData localDependencyData;
 	CPUDependencyData &hpDependencyData = (cpu != nullptr) ? cpu->getDependencyData() : localDependencyData;
+
+	if(task->isIf0()) {
+		Task *parent = task->getParent();
+		assert(parent != nullptr);
+
+		// Unlock parent that was waiting for this if0
+		Scheduler::addReadyTask(parent, cpu, UNBLOCKED_TASK_HINT);
+
+		// After adding a task, the CPUManager may want to un-idle CPUs
+		CPUManager::executeCPUManagerPolicy(cpu, ADDED_TASKS, 1);
+	}
 
 	if (task->markAsFinished(cpu)) {
 		DataAccessRegistration::unregisterTaskDataAccesses(
