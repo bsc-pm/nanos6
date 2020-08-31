a2e2f1309ce57bb7f2e9d81f0a49d7205d16028b
Kevin Sala <kevin.sala@bsc.es>
2020-08-31 12:55:30 +0200

    Fixing ticket array spinlock.

--- src/lowlevel/TicketArraySpinLock.hpp
+++ src/lowlevel/TicketArraySpinLock.hpp
@@ -8,6 +8,7 @@
 #define TICKET_ARRAY_SPIN_LOCK_HPP
 
 #include <atomic>
+#include <cstdint>
 
 #include "MemoryAllocator.hpp"
 #include "SpinWait.hpp"
@@ -15,11 +16,13 @@
 
 
 class TicketArraySpinLock {
-	// These are aligned on a cache line boundary in order to avoid false sharing:
-	alignas(CACHELINE_SIZE) Padded<std::atomic_size_t> *_buffer;
-	alignas(CACHELINE_SIZE) std::atomic_size_t _head;
-	alignas(CACHELINE_SIZE) size_t _next;
-	alignas(CACHELINE_SIZE) size_t _size;
+	typedef std::atomic<uint64_t> atomic_t;
+
+	//! These are aligned on a cache line boundary in order to avoid false sharing
+	alignas(CACHELINE_SIZE) Padded<atomic_t> *_buffer;
+	alignas(CACHELINE_SIZE) atomic_t _head;
+	alignas(CACHELINE_SIZE) uint64_t _next;
+	alignas(CACHELINE_SIZE) uint64_t _size;
 
 public:
 	TicketArraySpinLock(size_t size) :
@@ -27,37 +30,42 @@ public:
 		_next(0),
 		_size(size)
 	{
-		_buffer = (Padded<std::atomic_size_t> *) MemoryAllocator::alloc(_size * sizeof(Padded<std::atomic_size_t>));
+		_buffer = (Padded<atomic_t> *) MemoryAllocator::alloc(_size * sizeof(Padded<atomic_t>));
 		for (size_t i = 0; i < _size; i++) {
-			new (&_buffer[i]) Padded<std::atomic_size_t>(0);
+			new (&_buffer[i]) Padded<atomic_t>(0);
 		}
 	}
 
 	~TicketArraySpinLock()
 	{
-		MemoryAllocator::free(_buffer, _size * sizeof(Padded<std::atomic_size_t>));
-	}
-
-	inline bool tryLock()
-	{
-		const size_t head = _head.fetch_add(1, std::memory_order_relaxed);
-		const size_t idx = head % _size;
-		return !(_buffer[idx].load(std::memory_order_acquire) != head);
+		MemoryAllocator::free(_buffer, _size * sizeof(Padded<atomic_t>));
 	}
 
 	inline void lock()
 	{
-		const size_t head = _head.fetch_add(1, std::memory_order_relaxed);
-		const size_t idx = head % _size;
+		const uint64_t head = _head.fetch_add(1, std::memory_order_relaxed);
+		const uint64_t idx = head % _size;
 		while (_buffer[idx].load(std::memory_order_acquire) != head) {
 			spinWait();
 		}
-		_next = (head+1);
+	}
+
+	inline bool tryLock()
+	{
+		uint64_t head = _head.load(std::memory_order_relaxed);
+		const uint64_t idx = head % _size;
+		if (_buffer[idx].load(std::memory_order_relaxed) != head)
+			return false;
+
+		return std::atomic_compare_exchange_strong_explicit(
+			&_head, &head, head + 1,
+			std::memory_order_acquire,
+			std::memory_order_relaxed);
 	}
 
 	inline void unlock()
 	{
-		const size_t idx = _next % _size;
+		const uint64_t idx = ++_next % _size;
 		_buffer[idx].store(_next, std::memory_order_release);
 	}
 };
