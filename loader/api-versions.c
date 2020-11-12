9cc311929fa90c0b1e37a61503b0e56626c95f3e
Kevin Sala <kevin.sala@bsc.es>
2020-11-12 13:38:28 +0100

    Removing reduction storage API getter
    Now reduction storage is provided through the translation table
    instead of the nanos6_get_reduction_storage1 function. We also
    add a API version number (initialized to 2) for the reductions

--- loader/api-versions.c
+++ loader/api-versions.c
@@ -1,7 +1,7 @@
 /*
 	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
 
-	Copyright (C) 2018-2019 Barcelona Supercomputing Center (BSC)
+	Copyright (C) 2018-2020 Barcelona Supercomputing Center (BSC)
 */
 
 
@@ -25,6 +25,7 @@ nanos6_api_versions_t const __user_code_expected_nanos6_api_versions = {
 	.locking_api_version = nanos6_locking_api,
 	.monitoring_api_version = nanos6_monitoring_api,
 	.polling_api_version = nanos6_polling_api,
+	.reductions_api_version = nanos6_reductions_api,
 	.task_constraints_api_version = nanos6_task_constraints_api,
 	.task_execution_api_version = nanos6_task_execution_api,
 	.task_info_registration_api_version = nanos6_task_info_registration_api,
