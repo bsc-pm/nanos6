8c73ffa2e8c48112c6a90cbdf2f39a4788b7441d
Marcos Maroñas <marcos.maronasbravo@bsc.es>
2020-11-12 20:36:52 +0100

    Changing default value of several environment variables related to data tracking.

--- src/dependencies/DataTrackingSupport.hpp
+++ src/dependencies/DataTrackingSupport.hpp
@@ -13,13 +13,13 @@
 class Task;
 
 namespace DataTrackingSupport {
-	static EnvironmentVariable<bool> _trackingEnabled("NANOS6_DATA_TRACKING", 1);
-	static EnvironmentVariable<bool> _NUMATrackingEnabled("NANOS6_NUMA_TRACKING", 1);
-	static EnvironmentVariable<bool> _NUMASchedulingEnabled("NANOS6_NUMA_SCHEDULING", 1);
-	static EnvironmentVariable<bool> _NUMAStealingEnabled("NANOS6_NUMA_STEALING", 1);
+	static EnvironmentVariable<bool> _trackingEnabled("NANOS6_DATA_TRACKING", 0);
+	static EnvironmentVariable<bool> _NUMATrackingEnabled("NANOS6_NUMA_TRACKING", 0);
+	static EnvironmentVariable<bool> _NUMASchedulingEnabled("NANOS6_NUMA_SCHEDULING", 0);
+	static EnvironmentVariable<bool> _NUMAStealingEnabled("NANOS6_NUMA_STEALING", 0);
 	static EnvironmentVariable<std::string> _NUMATrackingType("NANOS6_NUMA_TRACKING_TYPE", "DEPS");
 	static EnvironmentVariable<bool> _trackingReportEnabled("NANOS6_TRACKING_REPORT", 0);
-	static EnvironmentVariable<bool> _checkExpiration("NANOS6_CHECK_EXPIRATION", 1);
+	static EnvironmentVariable<bool> _checkExpiration("NANOS6_CHECK_EXPIRATION", 0);
 
 	//! Data tracking
 	typedef int16_t location_t;
