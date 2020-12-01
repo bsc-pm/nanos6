d9dff685417e362148cbb31860b713e02ac77d81
Kevin Sala <kevin.sala@bsc.es>
2020-12-01 23:08:23 +0100

    Adding BlockingAPI::waitForUs function

--- src/system/BlockingAPI.hpp
+++ src/system/BlockingAPI.hpp
@@ -7,6 +7,8 @@
 #ifndef BLOCKING_API_HPP
 #define BLOCKING_API_HPP
 
+#include <cstdint>
+
 #include "tasks/Task.hpp"
 
 namespace BlockingAPI {
@@ -17,13 +19,23 @@ namespace BlockingAPI {
 	//! or runtime code
 	void blockCurrentTask(bool fromUserCode = false);
 
-	//! \brief Unblocks a previously blocked task
+	//! \brief Unblock a previously blocked task
 	//!
 	//! \param task The task about to be unblocked
 	//! \param fromUserCode Whether this function is called form user code
 	//! or runtime code
 	void unblockTask(Task *task, bool fromUserCode = false);
 
+	//! \brief Pause the current task for an amount of microseconds
+	//!
+	//! The task is paused for approximately the amount of microseconds
+	//! passed as a parameter. The runtime may choose to execute other
+	//! tasks within the execution scope of this call
+	//!
+	//! \param timeUs The time that should be spent while paused in us
+	//!
+	//! \returns The actual time spent during the pause
+	uint64_t waitForUs(uint64_t timeUs);
 }
 
 #endif // BLOCKING_API_HPP
