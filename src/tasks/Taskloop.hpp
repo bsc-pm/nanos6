072439f320b9f9d7cccb01f2e6cf96300f13d9fa
David Alvarez <david.alvarez@bsc.es>
2020-12-04 12:17:48 +0100

    Handle taskloop case as well

--- src/tasks/Taskloop.hpp
+++ src/tasks/Taskloop.hpp
@@ -87,7 +87,7 @@ public:
 		return (_bounds.upper_bound - _bounds.lower_bound);
 	}
 
-	void body(nanos6_address_translation_entry_t * = nullptr) override;
+	void body(nanos6_address_translation_entry_t *translationTable) override;
 
 	inline void registerDependencies(bool discrete = false) override
 	{
