0cee4b82c5f8550a7d958579d916281cea60c9aa
Antoni Navarro <anavarr1@bsc.es>
2020-12-23 15:58:08 +0100

    Modify the generate config script
    Order does not matter anymore, and we handle specific sections instead
    of ones and zeros

--- scripts/generate_config.sh
+++ scripts/generate_config.sh
@@ -1,57 +1,69 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 #	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
 #
 #	Copyright (C) 2020 Barcelona Supercomputing Center (BSC)
 #
-# Generate config depending on enabled flags.
+# Generate config depending on enabled flags
 
 # Stop on error
 set -e
 
 # Remove a single config section
 remove_section () {
-	contents=$1
+	file=$1
 	section=$2
 
-	# Remove anything between the __require_{section} and
-	# __!require_{section}, inclusive
-	printf "%s\n" "${contents}" | \
-		awk '/^__require_'"${section}"'$/ { skip=1; next }; \
-			/^__!require_'"${section}"'$/{skip=0; next}; \
-			!skip{print}'
+	# Remove anything between "__require_{section}" and "__!require_{section}"
+	printf "%s\n" "${file}" | sed "/^__require_${section}/,/^__!require_${section}/d"
 }
 
 # Remove dangling requires and newlines
 remove_unused_requires () {
-	contents=$1
+	file=$1
 
-	# Remove any __require_* or __!require_* leftover lines
-	# and contract multiple newlines into one with awk
-	printf "%s\n" "${contents}" | sed '/^__!\?require_.*$/d' | \
-		awk '!NF {f=1; next} f {print ""; f=0} 1'
+	# Remove any __require_* or __!require_* leftover lines and
+	# merge multiple newlines into a single one (last sed)
+	printf "%s\n" "${file}" | sed '/^__.*require_.*/d' | \
+		sed 'N;/^\n$/D;P;D;'
 }
 
-template_file=$1
-shift
+# Retreive the template file
+if test -f $1; then
+	template_file=$1
+else
+	printf "Error: When reading Nanos6 Template Config File\n"
+	exit
+fi
 
-# This sections must be in order with the arguments of the script
-possible_sections=(CUDA OPENACC CLUSTER DLB CTF GRAPH VERBOSE EXTRAE PAPI PQOS)
+# Extract a list of all the possible sections from the template file
+possible_sections=($(cat $template_file | sed -n 's/^__require_//p'))
+
+# Iterate all the possible sections and if one is not found in parameters, disable it
 disabled_sections=()
 
-# Read the arguments using the possible_sections order to check which sections
-# are disabled and must be removed
-for section in "${possible_sections[@]}"; do
-	if [ "$1" != "1" ]; then
-		disabled_sections+=(${section})
+# We begin with the second parameter, as the first one is the template file
+i=2
+while [ $i -le $# ]; do
+	candidate=${!i}
+	found=0
+	for section in $@; do
+		if [ $candidate == $section ]; then
+			found=1
+			break
+		fi
+	done
+
+	if [ !$found ]; then
+		disabled_sections+=($section)
 	fi
-	shift
+
+	# Check next section
+	i=$((i+1))
 done
 
-# Read configure template
-file_contents=$(cat ${template_file})
-
 # For each disabled section, call remove_section
+file_contents=$(cat ${template_file})
 for section in ${disabled_sections[@]}; do
 	file_contents=$(remove_section "${file_contents}" "${section}")
 done
@@ -60,4 +72,4 @@ done
 file_contents=$(remove_unused_requires "${file_contents}")
 
 # Return the processed file
-printf "%s\n" "${file_contents}"
+printf "%s\n" "$file_contents"
