f3f16b1a5e0d38928231cb5d16c935c91f14e6be
Kevin Sala <kevin.sala@bsc.es>
2021-01-11 14:35:29 +0100

    Updating copyright years

--- src/hardware/hwinfo/DeviceInfo.hpp
+++ src/hardware/hwinfo/DeviceInfo.hpp
@@ -1,7 +1,7 @@
 /*
 	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
 
-	Copyright (C) 2015-2020 Barcelona Supercomputing Center (BSC)
+	Copyright (C) 2015-2021 Barcelona Supercomputing Center (BSC)
 */
 
 #ifndef DEVICE_INFO_HPP
