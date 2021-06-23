3bb02d2af0d93019c698c5b44985885584461fcf
Kevin Sala <kevin.sala@bsc.es>
2021-06-23 17:35:34 +0200

    Fixing debug instrumentation header

--- src/instrument/api/InstrumentDebug.hpp
+++ src/instrument/api/InstrumentDebug.hpp
@@ -1,20 +1,22 @@
 /*
 	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
 
-	Copyright (C) 2020 Barcelona Supercomputing Center (BSC)
+	Copyright (C) 2020-2021 Barcelona Supercomputing Center (BSC)
 */
 
-#ifndef INSTRUMENT_POLLING_SERVICES_HPP
-#define INSTRUMENT_POLLING_SERVICES_HPP
+#ifndef INSTRUMENT_DEBUG_HPP
+#define INSTRUMENT_DEBUG_HPP
 
 #include <cstdint>
 
 namespace Instrument {
 
-	void pollingServiceEnter(uint8_t id);
-	void pollingServiceExit();
-	inline void pollingServiceRegister(const char *name, uint8_t id);
+	void debugRegister(const char *name, uint8_t id);
+
+	void debugEnter(uint8_t id);
+	void debugTransition(uint8_t id);
+	void debugExit();
 
 }
 
-#endif //INSTRUMENT_POLLING_SERVICES_HPP
+#endif // INSTRUMENT_DEBUG_HPP
