e2e21fc7f338bd85076dab95a8e5b387110a85f9
Antoni Navarro <anavarr1@bsc.es>
2021-06-29 18:16:58 +0200

    Revert "Setting the idle policy as the default again"
    This reverts commit d66484af8c99cffb19b245b810648509cd154ace.

--- src/executors/threads/cpu-managers/default/DefaultCPUManager.cpp
+++ src/executors/threads/cpu-managers/default/DefaultCPUManager.cpp
@@ -45,10 +45,10 @@ void DefaultCPUManager::preinitialize()
 	if (policyValue == "busy") {
 		_cpuManagerPolicy = new BusyPolicy();
 		_policyId = BUSY_POLICY;
-	} else if (policyValue == "idle" || policyValue == "default") {
+	} else if (policyValue == "idle" ) {
 		_cpuManagerPolicy = new IdlePolicy(numCPUs);
 		_policyId = IDLE_POLICY;
-	} else if (policyValue == "hybrid") {
+	} else if (policyValue == "hybrid" || policyValue == "default") {
 		_cpuManagerPolicy = new HybridPolicy(numCPUs);
 		_policyId = HYBRID_POLICY;
 	} else {
