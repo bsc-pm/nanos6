7a988516492565dde3f5aa0440bc0246c8d0fc8b
Kevin Sala <kevin.sala@bsc.es>
2021-08-31 12:54:56 +0200

    Fixing missing include in hardware counters

--- src/hardware-counters/pqos/PQoSHardwareCounters.cpp
+++ src/hardware-counters/pqos/PQoSHardwareCounters.cpp
@@ -1,11 +1,12 @@
 /*
 	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
 
-	Copyright (C) 2019-2020 Barcelona Supercomputing Center (BSC)
+	Copyright (C) 2019-2021 Barcelona Supercomputing Center (BSC)
 */
 
 #include <algorithm>
 #include <cassert>
+#include <string>
 #include <sys/utsname.h>
 
 #include "PQoSHardwareCounters.hpp"
