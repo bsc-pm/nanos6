fd9bb61d5cb7e45785369c49ffb9dcae6e1d9a13
Kevin Sala <kevin.sala@bsc.es>
2021-09-09 18:27:15 +0200

    Fixing the setting of DAZ optimization bit

--- src/lowlevel/TurboSettings.hpp
+++ src/lowlevel/TurboSettings.hpp
@@ -1,13 +1,14 @@
 /*
 	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
 
-	Copyright (C) 2020 Barcelona Supercomputing Center (BSC)
+	Copyright (C) 2020-2021 Barcelona Supercomputing Center (BSC)
 */
 
 #ifndef TURBO_SETTINGS_HPP
 #define TURBO_SETTINGS_HPP
 
 #ifdef __SSE2__
+#include <pmmintrin.h>
 #include <xmmintrin.h>
 #endif
 
@@ -22,11 +23,12 @@ public:
 		ConfigVariable<bool> turbo("turbo.enabled");
 
 		if (turbo.getValue()) {
-			// Enable flush-to-zero (FZ) and denormals are zero (FAZ) floating-point optimizations
-			// All threads created by the runtime system will inherit these optimization flags
-			const unsigned int mask = 0x8080;
-			unsigned int mxcsr = _mm_getcsr() | mask;
-			_mm_setcsr(mxcsr);
+			// Enable flush-to-zero (FZ) and denormals are zero (FAZ) floating-point
+			// optimizations if available. All threads created by the runtime system
+			// will inherit these optimization flags
+			_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+
+			_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
 		}
 #endif
 	}
