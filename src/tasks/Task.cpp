a5c51fa3b48bd40312cf63ad0933c142aa6541be
Kevin Sala <kevin.sala@bsc.es>
2022-03-03 22:41:56 +0100

    Reporting error when taskfor has onready clause
    Taskfors do not support the onready clause yet

--- src/tasks/Task.cpp
+++ src/tasks/Task.cpp
@@ -1,13 +1,14 @@
 /*
 	This file is part of Nanos6 and is licensed under the terms contained in the COPYING file.
 
-	Copyright (C) 2021 Barcelona Supercomputing Center (BSC)
+	Copyright (C) 2021-2022 Barcelona Supercomputing Center (BSC)
 */
 
 #include "nanos6.h"
 
 #include "Task.hpp"
 #include "executors/threads/WorkerThread.hpp"
+#include "lowlevel/FatalErrorHandler.hpp"
 
 
 void Task::runOnready(WorkerThread *currentThread)
@@ -15,6 +16,10 @@ void Task::runOnready(WorkerThread *currentThread)
 	assert(currentThread != nullptr);
 	assert(_taskInfo != nullptr);
 	assert(_taskInfo->onready_action != nullptr);
+
+	if (isTaskfor()) {
+		FatalErrorHandler::fail("Taskfors do not support onready action clause");
+	}
 
 	Task *currentTask = currentThread->unassignTask();
 	currentThread->setTask(this);
