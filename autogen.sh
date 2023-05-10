55fc753ad7bd1bdd3759194a2c95e905714ce98f
Raúl Peñacoba Veigas <rpenacob@bsc.es>
2023-05-10 15:02:01 +0000

    Remove hwloc folder, use tar instead. Add autogen.sh to handle untar

--- autogen.sh
+++ autogen.sh
@@ -3,9 +3,19 @@
 #
 #	Copyright (C) 2023 Barcelona Supercomputing Center (BSC)
 
+# Version of hwloc. Change this to update hwloc.
+hwloc_version=2.3.0
+
 if ! command -v autoreconf; then
 	echo "error: autoreconf not found"
 	exit 1
 fi
 
+if [ ! -d "hwloc-${hwloc_version}" ]
+then
+  mkdir hwloc-${hwloc_version}
+  tar -zxvf hwloc-${hwloc_version}.tar.gz --strip-components=1 -C hwloc-${hwloc_version}
+fi
+
+hwloc-${hwloc_version}/autogen.sh
 autoreconf -fiv
