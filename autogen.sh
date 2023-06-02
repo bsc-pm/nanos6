5ef82ac7e92ce124ba8b3cb43fda511c54537b7d
Kevin Sala <kevin.sala@bsc.es>
2023-06-02 09:23:20 +0200

    Improving embedded jemalloc mechanism

--- autogen.sh
+++ autogen.sh
@@ -4,62 +4,128 @@
 #	Copyright (C) 2023 Barcelona Supercomputing Center (BSC)
 
 # Folders where the dependency packages should be located
-deps_folder=deps
-hwloc_folder=${deps_folder}/hwloc
+deps_directory=deps
+hwloc_directory=${deps_directory}/hwloc
+jemalloc_directory=${deps_directory}/jemalloc
 
-# The default hwloc version. This value should be changed when updating the default hwloc
+# The default embedded dependency versions. This value should be changed when
+# updating the default versions
 hwloc_default_version=2.9.1
+jemalloc_default_version=5.3.0
 
+# Print the script usage and help
 usage() {
-	echo "Usage: $1 [--embed-hwloc VERSION] [--help]"
+	echo "Usage: $1 [--embed-hwloc VERSION] [--embed-jemalloc VERSION] [--help]"
 	echo ""
 	echo "  Description:"
-	echo "    The script extracts and embeds the external dependencies in the '${deps_folder}' folder, prepares"
+	echo "    The script extracts and embeds the external dependencies in the '${deps_base_directory}' directory, prepares"
 	echo "    autotools for these packages, and then, runs the autoreconf for the current package. The"
 	echo "    current default versions for the embedded dependencies are:"
 	echo "      - hwloc: ${hwloc_default_version}"
+	echo "      - jemalloc: ${jemalloc_default_version}"
 	echo ""
 	echo "  Options:"
-	echo "    --embed-hwloc VERSION  Embed the hwloc sources in '${hwloc_folder}' with the '${hwloc_folder}-VERSION.tar.gz'"
-	echo "                           tarball. The VERSION value can be 'default' to use the default hwloc version"
-	echo "    --help, -h             Show the current help and exit"
+	echo "    --embed-hwloc VERSION     Embed the hwloc sources in '${hwloc_directory}' with the '${hwloc_directory}-VERSION.tar.gz'"
+	echo "                              tarball. The VERSION value can be 'default' to use the default hwloc version"
+	echo "    --embed-jemalloc VERSION  Embed the jemalloc sources in '${jemalloc_directory}' with the '${jemalloc_directory}-VERSION.tar.gz'"
+	echo "                              tarball. The VERSION value can be 'default' to use the default jemalloc version"
+	echo "    --help, -h                Show the current help and exit"
 }
 
+# Print an error and abort execution
 error() {
 	echo "==== ERROR: $@"
 	exit 1
 }
 
+# Print verbose information
 verbose() {
 	echo "==== INFO: $@"
 }
 
-# Verify if the script is being is executed from the top level directory
+# Return whether a file exist
+exist_command() {
+	command -v $1 >/dev/null 2>&1
+	return $?
+}
+
+# Check whether a file exist and abort otherwise
+check_file() {
+	if [ ! -f $1 ]; then
+		error "$1 not found"
+	fi
+}
+
+# Check whether a command exists and abort otherwise
+check_command() {
+	if ! exist_command $1 ; then
+		error "$1 command not found"
+	fi
+}
+
+# Extract a package in a specific directory
+extract_package() {
+	local directory=$1
+	local tarball=$2
+	local regenerate=$3
+
+	if [ ! -d "${directory}" ] || [ ${regenerate} -eq 1 ]; then
+		rm -rf ${directory}
+		mkdir ${directory}
+		tar -zxvf ${tarball} --strip-components=1 -C ${directory}
+		verbose "successfully extracted '${tarball}' in '${directory}'"
+	else
+		verbose "reusing previously extracted '${directory}'"
+	fi
+}
+
+# Prepare autotools for a package in a specific directory
+autogen_package() {
+	local directory=$1
+	local autogen=$2
+
+	currdir=$(pwd)
+	cd ${directory}
+
+	if exist_command $autogen ; then
+		verbose "generating autotools from '${directory}' package"
+		${autogen}
+	else
+		verbose "$autogen command or script not found in '${directory}'"
+		verbose "skipping autotools generation from '${directory}' package"
+	fi
+
+	cd ${currdir}
+}
+
+# Verify whether the script is being is executed from the top level directory
 if [ ! -f src/scheduling/Scheduler.hpp ]; then
 	error "must execute $0 at the top level directory"
 fi
-
-if ! command -v tar ; then
-	error "tar command not found"
-fi
-
-if ! command -v autoreconf ; then
-	error "autoreconf command not found"
-fi
+currdir=$(pwd)
 
 # If not specified, use the default version distributed along with the runtime version
 hwloc_version="default"
+jemalloc_version="default"
 
 # Do not regenerate embedded dependencies by default
 hwloc_regenerate=0
+jemalloc_regenerate=0
 
 while [ $# -gt 0 ]; do
 	if [ "$1" = "--embed-hwloc" ]; then
 		if [ $# -lt 2 ]; then
-			error "option --embed-hwloc requires the version number or 'default'"
+			error "option $1 requires the version number or 'default'"
 		fi
 		hwloc_version=$2
 		hwloc_regenerate=1
+		shift 2
+	elif [ "$1" = "--embed-jemalloc" ]; then
+		if [ $# -lt 2 ]; then
+			error "option $1 requires the version number or 'default'"
+		fi
+		jemalloc_version=$2
+		jemalloc_regenerate=1
 		shift 2
 	elif [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 		usage $0
@@ -70,41 +136,37 @@ while [ $# -gt 0 ]; do
 	fi
 done
 
+check_command "tar"
+check_command "autoreconf"
+check_command "autoconf"
+
 # Set the proper version if the user requested the default
-if [ "${hwloc_version}" = "default" ] || [ "${hwloc_version}" = "DEFAULT" ]; then
+if [ "${hwloc_version}" = "default" ]; then
 	hwloc_version=${hwloc_default_version}
 fi
-
-hwloc_tarball=${hwloc_folder}-${hwloc_version}.tar.gz
-if [ ! -f "${hwloc_tarball}" ]; then
-	error "${hwloc_tarball} not found"
+if [ "${jemalloc_version}" = "default" ]; then
+	jemalloc_version=${jemalloc_default_version}
 fi
 
-rm -rf deps/jemalloc-5.3.0
-mkdir deps/jemalloc-5.3.0
-tar -zxvf deps/jemalloc-5.3.0.tar.gz --strip-components=1 -C deps/jemalloc-5.3.0
-(cd deps/jemalloc-5.3.0 && ./autogen.sh)
+hwloc_tarball=${hwloc_directory}-${hwloc_version}.tar.gz
+jemalloc_tarball=${jemalloc_directory}-${jemalloc_version}.tar.gz
 
-if [ ! -d "${hwloc_folder}" ] || [ ${hwloc_regenerate} -eq 1 ]; then
-	rm -rf ${hwloc_folder}
-	mkdir ${hwloc_folder}
-	tar -zxvf ${hwloc_tarball} --strip-components=1 -C ${hwloc_folder}
-	verbose "successfully extracted '${hwloc_tarball}' in '${hwloc_folder}'"
-else
-	verbose "reusing previously extracted hwloc in '${hwloc_folder}'"
-fi
+# Check that the package tarballs are available
+check_file ${hwloc_tarball}
+check_file ${jemalloc_tarball}
 
-if [ -f ${hwloc_folder}/autogen.sh ]; then
-	verbose "generating autotools from '${hwloc_folder}' package"
-	./${hwloc_folder}/autogen.sh
-else
-	verbose "autogen.sh script not found in '${hwloc_folder}'"
-	verbose "skipping autotools generation from '${hwloc_folder}' package"
-fi
+# Extract package tarballs if needed
+extract_package ${hwloc_directory} ${hwloc_tarball} ${hwloc_regenerate}
+extract_package ${jemalloc_directory} ${jemalloc_tarball} ${jemalloc_regenerate}
 
-if [ ! -f ${hwloc_folder}/configure ]; then
-	error "${hwloc_folder}/configure not found"
-fi
+# Prepare autotools for each package
+autogen_package ${hwloc_directory} "./autogen.sh"
+autogen_package ${jemalloc_directory} "autoconf"
 
+# Check their configure scripts exist
+check_file "${hwloc_directory}/configure"
+check_file "${jemalloc_directory}/configure"
+
+# Prepare autotools for the Nanos6 package
 verbose "configuring autotools from current package"
 autoreconf -fiv
